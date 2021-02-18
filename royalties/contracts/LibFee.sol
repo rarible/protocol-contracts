85012606d14a0fee41cddcd5b800d92513791dcd
Evgeny Nacu <evgeny.nacu@gmail.com>
2021-02-18 14:25:19 +0300

    fix(*): hash for Fee

--- royalties/contracts/LibFee.sol
+++ royalties/contracts/LibFee.sol
@@ -3,8 +3,14 @@
 pragma solidity >=0.6.2 <0.8.0;
 
 library LibFee {
+    bytes32 public constant FEE_TYPE_HASH = keccak256("Fee(address account,uint256 value)");
+
     struct Fee {
         address payable account;
         uint value;
+    }
+
+    function hash(Fee memory fee) internal pure returns (bytes32) {
+        return keccak256(abi.encode(FEE_TYPE_HASH, fee.account, fee.value));
     }
 }
