27d2b22553285da55dfd427dec58e690a37d078a
Evgeny Nacu <evgeny.nacu@gmail.com>
2021-02-25 09:56:30 +0300

    fix(*): fix for RoyaltiesV2 interface id

--- royalties/contracts/LibRoyaltiesV2.sol
+++ royalties/contracts/LibRoyaltiesV2.sol
@@ -4,10 +4,7 @@ pragma solidity >=0.6.2 <0.8.0;
 
 library LibRoyaltiesV2 {
     /*
-     * bytes4(keccak256('getFeeBps(uint256)')) == 0x0ebd4c7f
-     * bytes4(keccak256('getFeeRecipients(uint256)')) == 0xb9c4d9fb
-     *
-     * => 0x0ebd4c7f ^ 0xb9c4d9fb == 0xb7799584
+     * bytes4(keccak256('getFees(uint256)')) == 0xd5a06d4c
      */
-    bytes4 constant _INTERFACE_ID_FEES = 0xb7799584;
+    bytes4 constant _INTERFACE_ID_FEES = 0xd5a06d4c;
 }
