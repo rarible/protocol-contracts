4742da974f1669e4e3bd2e74a778760cfbf12e03
Evgeny Nacu <evgeny.nacu@gmail.com>
2021-03-16 09:18:29 +0300

    fix(*): erc1271

---
+++ tokens-test/contracts/ERC1271.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.6.2 <0.8.0;
+
+abstract contract ERC1271 {
+    bytes4 constant public ERC1271_INTERFACE_ID = 0xfb855dc9; // this.isValidSignature.selector
+
+    bytes4 constant public ERC1271_RETURN_VALID_SIGNATURE =   0x20c13b0b;
+    bytes4 constant public ERC1271_RETURN_INVALID_SIGNATURE = 0x00000000;
+
+    /**
+    * @dev Function must be implemented by deriving contract
+    * @param _hash Arbitrary length data signed on the behalf of address(this)
+    * @param _signature Signature byte array associated with _data
+    * @return A bytes4 magic value 0x20c13b0b if the signature check passes, 0x00000000 if not
+    *
+    * MUST NOT modify state (using STATICCALL for solc < 0.5, view modifier for solc > 0.5)
+    * MUST allow external calls
+    */
+    function isValidSignature(bytes32 _hash, bytes memory _signature) public virtual view returns (bytes4);
+
+    function returnIsValidSignatureMagicNumber(bool isValid) internal pure returns (bytes4) {
+        return isValid ? ERC1271_RETURN_VALID_SIGNATURE : ERC1271_RETURN_INVALID_SIGNATURE;
+    }
+}
