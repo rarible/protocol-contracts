9ee74e0b477373cc4ea7a413cab6e9c352fe77e9
Evgeny Nacu <evgeny.nacu@gmail.com>
2021-03-19 22:55:38 +0300

    chore(*): deploy lazy mint contracts to rinkeby

--- exchange-v2/contracts/roles/OperatorRole.sol
+++ exchange-v2/contracts/roles/OperatorRole.sol
@@ -7,6 +7,11 @@ import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 contract OperatorRole is OwnableUpgradeable {
     mapping (address => bool) operators;
 
+    function __OperatorRole_init() external initializer {
+        __Context_init_unchained();
+        __Ownable_init_unchained();
+    }
+
     function addOperator(address operator) external onlyOwner {
         operators[operator] = true;
     }
