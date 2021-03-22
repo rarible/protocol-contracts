c7b3fb4cd285f9022f1e6ecab32fcb64acf43e3f
Konstantin Sherbakov <sherbakov.konst@yandex.ru>
2021-03-22 23:33:06 +0700

    feat(*): remove getRoyalties from RaribleTransferManager

--- exchange-v2/contracts/exchange/v2/RaribleTransferManager.sol
+++ exchange-v2/contracts/exchange/v2/RaribleTransferManager.sol
@@ -10,15 +10,11 @@ import "./LibOrderDataV1.sol";
 import "./ITransferManager.sol";
 import "./TransferExecutor.sol";
 import "./LibAsset.sol";
-import "@rarible/royalties/contracts/RoyaltiesV1.sol";
-import "@rarible/royalties/contracts/LibRoyaltiesV2.sol";
-import "@rarible/royalties/contracts/LibRoyaltiesV1.sol";
-import "@rarible/royalties/contracts/impl/RoyaltiesV1Impl.sol";
-import "@rarible/royalties/contracts/impl/RoyaltiesV2Impl.sol";
+import "./RoyaltiesRegistry.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC721/IERC721Upgradeable.sol";
 import "../../utils/BpLibrary.sol";
 
-abstract contract RaribleTransferManager is OwnableUpgradeable, ITransferManager {
+abstract contract RaribleTransferManager is OwnableUpgradeable, ITransferManager, RoyaltiesRegistry{
     using BpLibrary for uint;
     using SafeMathUpgradeable for uint;
 
@@ -123,7 +119,10 @@ abstract contract RaribleTransferManager is OwnableUpgradeable, ITransferManager
         address from,
         bytes4 transferDirection
     ) internal returns (uint restValue){
-        LibPart.Part[] memory fees = getRoyalties(matchNft);
+        //todo detect token1, tokId1
+        address token1;
+        uint tokId1;
+        LibPart.Part[] memory fees = RoyaltiesRegistry.getRoyalties(token1, tokId1, matchNft);
         restValue = rest;
         for (uint256 i = 0; i < fees.length; i++) {
             (uint newRestValue, uint feeValue) = subFeeInBp(restValue, amount, fees[i].value);
@@ -217,42 +216,6 @@ abstract contract RaribleTransferManager is OwnableUpgradeable, ITransferManager
             newValue = 0;
             realFee = value;
         }
-    }
-
-    function getRoyalties(LibAsset.AssetType memory asset) internal view returns (LibPart.Part[] memory feesRecipients) {
-        if (asset.tp != LibAsset.ERC1155_ASSET_TYPE && asset.tp != LibAsset.ERC721_ASSET_TYPE) {
-            return feesRecipients;
-        }
-        (address addressAsset, uint tokenIdAsset) = abi.decode(asset.data, (address, uint));
-        if (IERC165Upgradeable(addressAsset).supportsInterface(LibRoyaltiesV2._INTERFACE_ID_FEES)) {
-            RoyaltiesV2Impl withFees = RoyaltiesV2Impl(addressAsset);
-            try withFees.getRoyalties(tokenIdAsset) returns (LibPart.Part[] memory feesRecipientsResult) {
-                feesRecipients = feesRecipientsResult;
-            } catch {}
-        } else if (IERC165Upgradeable(addressAsset).supportsInterface(LibRoyaltiesV1._INTERFACE_ID_FEES)) {
-            RoyaltiesV1Impl withFees = RoyaltiesV1Impl(addressAsset);
-            address payable[] memory recipients;
-            try withFees.getFeeRecipients(tokenIdAsset) returns (address payable[] memory recipientsResult) {
-                recipients = recipientsResult;
-            } catch {
-                return feesRecipients;
-            }
-            uint[] memory fees;
-            try withFees.getFeeBps(tokenIdAsset) returns (uint[] memory feesResult) {
-                fees = feesResult;
-            } catch {
-                return feesRecipients;
-            }
-            if (fees.length != recipients.length) {
-                return feesRecipients;
-            }
-            feesRecipients = new LibPart.Part[](fees.length);
-            for (uint256 i = 0; i < fees.length; i++) {
-                feesRecipients[i].value = fees[i];
-                feesRecipients[i].account = recipients[i];
-            }
-        }
-        return feesRecipients;
     }
 
 
