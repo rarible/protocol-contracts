7325f634dec0545b110841b6b8400746a76e4691
Konstantin Sherbakov <sherbakov.konst@yandex.ru>
2021-03-24 12:47:36 +0700

    test(*): add test

---
+++ exchange-v2/test-royalty-registry.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+#Test metods in exchange-v2/contracts/exchange/v2/RoyaltiesRegistry.sol
+truffle test \
+            ./test/v2/RoyaltiesRegistry.test.js
+
+
+
+
---
+++ exchange-v2/test/v2/RoyaltiesRegistry.test.js
@@ -0,0 +1,27 @@
+const { deployProxy, upgradeProxy } = require('@openzeppelin/truffle-upgrades');
+const RoyaltiesRegistry = artifacts.require("RoyaltiesRegistry.sol");
+
+const { Order, Asset, sign } = require("../order");
+const EIP712 = require("../EIP712");
+const ZERO = "0x0000000000000000000000000000000000000000";
+const { expectThrow, verifyBalanceChange } = require("@daonomic/tests-common");
+const { ETH, ERC20, ERC721, ERC1155, ORDER_DATA_V1, TO_MAKER, TO_TAKER, PROTOCOL, ROYALTY, ORIGIN, PAYOUT, enc, id } = require("../assets");
+
+contract("RoyaltiesRegistry, test metods", accounts => {
+	let testing;
+	let protocol = accounts[9];
+	let community = accounts[8];
+	const eth = "0x0000000000000000000000000000000000000000";
+
+	beforeEach(async () => {
+		testing = await RoyaltiesRegistry.new();
+	});
+
+	describe("Metods works:", () => {
+		it("setTokenRoyalties()", async () => {
+ 			console.log("setTokenRoyalties() works!");
+    })
+
+	})
+
+});
