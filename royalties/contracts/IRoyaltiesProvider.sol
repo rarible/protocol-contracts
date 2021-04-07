99afef9b9f17d3a9b6e93691b60533b4ae635c43
Evgeny Nacu <evgeny.nacu@gmail.com>
2021-04-07 22:35:03 +0300

    fix(*): added IRoyaltiesProvider to @rarible/royalties

---
+++ royalties/contracts/IRoyaltiesProvider.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.6.2 <0.8.0;
+
+import "./LibPart.sol";
+
+interface IRoyaltiesProvider {
+    function getRoyalties(address token, uint tokenId) external returns (LibPart.Part[] memory);
+}
