1d1c0417ea5a0b0561e05858c3af42bfc289b9e1
Konstantin Sherbakov <sherbakov.konst@yandex.ru>
2021-04-12 12:01:18 +0700

    test(*): replace ExchangeSimpleV2_1.sol

--- exchange-v2/test/contracts/v2/ExchangeSimpleV2_1.sol
+++ exchange-v2/test/contracts/v2/ExchangeSimpleV2_1.sol
@@ -3,7 +3,7 @@
 pragma solidity >=0.6.2 <0.8.0;
 pragma abicoder v2;
 
-import "./ExchangeSimpleV2.sol";
+import "../../../contracts/exchange/v2/ExchangeSimpleV2.sol";
 
 contract ExchangeSimpleV2_1 is ExchangeSimpleV2 {
     function getSomething() external pure returns (uint) {
