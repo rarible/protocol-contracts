303d67e94d059c634a9dd81be7e38cfb0a5ab151
Konstantin Sherbakov <sherbakov.konst@yandex.ru>
2021-04-14 16:19:46 +0700

    test(*): improve test for royalties-registry

--- royalties-registry/test-royalty-registry-simple.sh
+++ royalties-registry/test-royalty-registry-simple.sh
@@ -3,6 +3,8 @@
 truffle test \
             ./test/contracts/RoyaltiesRegistrySimple.test.js \
             ./test/contracts/royalty-registry/RoyaltiesProviderTest.sol \
+            ./test/contracts/royalty-registry/RoyaltiesRegistryTest.sol \
+            ./test/contracts/tokens/TestERC721.sol \
             ./test/contracts/tokens/TestERC721WithRoyaltyV1OwnableUpgradeable.sol
 
 
