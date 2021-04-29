d03dcc02f7c46bd075207cf42fb4be1ae3980cef
Konstantin Sherbakov <sherbakov.konst@yandex.ru>
2021-04-29 21:31:43 +0700

    refactoring(*): delete unnecessary, add 4 basis methods.

--- staking/contracts/Staking.sol
+++ staking/contracts/Staking.sol
@@ -21,100 +21,67 @@ contract Staking {
     uint256 constant STARTING_POINT_WEEK = 2676;    //starting point week (Staking Epoch begining)
     ERC20Upgradeable public token;
 
-    struct Lock {
-        uint dt;        //deposit time, period
-        uint amount;    //amount deposited, num ERC20
-        uint et;        //end time, timestamp in WEEKs
-        uint cliff;     //cliff - period when slope = 0
-    }
-
     struct Locks {
-        mapping (uint => Lock) userLocks;   //idLock - Lock (end time)
-        uint balance;                       //user Balanse in erc20
+        BrokenLineDomain.BrokenLine balance;   //line of stRari balance
+        BrokenLineDomain.BrokenLine locked;    //locked amount (RARI)
+        uint amount;                           //user Balanse in erc20
     }
 
-    mapping(address => BrokenLineDomain.BrokenLine) public userBalances;    //address - line
-    BrokenLineDomain.BrokenLine public totalBalances;                       //total User Balance
-    mapping (address => Locks) locks;                                       //address User - Lock
-    Locks public totalLocks;
-    uint public idLock;
+    mapping (address => Locks) locks;        //address User - Lock
+    BrokenLineDomain.BrokenLine public totalSupplyLine; //total stRARI balance
 
     constructor(ERC20Upgradeable _token) public {
         token = _token;
-        idLock = 1;
     }
 
-    function createLock(address account, uint amount, uint slope, uint cliff) public returns (uint) {
+    function createLock(address account, uint amount, uint slope, uint cliff) public {
         //todo проверки
         uint blockTime = roundTimestamp(block.timestamp);
         uint period = amount/slope;
-        BrokenLineDomain.Line memory line = BrokenLineDomain.Line(blockTime, amount, slope);
-        idLock++;
-        userBalances[account].add(line, cliff);
-        totalBalances.add(line, cliff);
-        locks[account].userLocks[idLock] = Lock(blockTime, amount, blockTime + period, cliff);
-        totalLocks.userLocks[idLock] = Lock(blockTime, amount, blockTime + period, cliff);
-        locks[account].balance += amount;
-        totalLocks.balance += amount;
+        BrokenLineDomain.Line memory line = BrokenLineDomain.Line(blockTime, getStake(amount, slope, cliff), slope);
+        BrokenLineDomain.Line memory lineLocked = BrokenLineDomain.Line(blockTime, amount, slope);
+
+        totalSupplyLine.add(line, cliff);
+        locks[account].balance.add(line, cliff);
+        locks[account].locked.add(lineLocked, cliff);
+        locks[account].amount += amount;
         require(token.transferFrom(account, address(this), amount), "failure while transferring");
-        return idLock;
 
         // как меняется lock общий, когда юзер приходит/уходит/меняет
         // 1. нужно применить пропущенные изменения (окончания локов)
         // 2. если добавляем, то
     }
 
-    /*
-    * сколько залочено всего
-    */
     function totalSupply() public returns (uint) {
-        if (totalBalances.initial.start == 0) { //no lock
+        if (totalSupplyLine.initial.start == 0) { //no lock
             return 0;
         }
-        totalBalances.update(roundTimestamp(block.timestamp));
-        return totalBalances.initial.bias;
+        totalSupplyLine.update(roundTimestamp(block.timestamp));
+        return totalSupplyLine.initial.bias;
     }
 
-    /*
-	* сколько залочено у одного User
-	*/
-    function userSypply(address account) public returns (uint) {
-        if (userBalances[account].initial.start == 0) { //no lock
+    function balanceOf(address account) public returns (uint) {
+        if (locks[account].balance.initial.start == 0) { //no lock
             return 0;
         }
-        userBalances[account].update(roundTimestamp(block.timestamp));
-        return userBalances[account].initial.bias;
+        locks[account].balance.update(roundTimestamp(block.timestamp));
+        return locks[account].balance.initial.bias;
     }
 
-    /*
-    * сколько мы можем одному User перечислить ERC20 обратно
-    */
-    function userWithdrawSupply(address account) public returns(uint) {
-        userBalances[account].update(roundTimestamp(block.timestamp));
-        return locks[account].balance - userBalances[account].initial.bias;
-    }
-
-    /*
-    * сколько мы можем всем перечислить ERC20 обратно
-    */
-    function totalWithdrawSupply(address account) public returns(uint) {
-        totalBalances.update(roundTimestamp(block.timestamp));
-        return totalLocks.balance - totalBalances.initial.bias;
-    }
-
-    /*
-    * одному User перечислить ERC20 обратно
-    */
-    function userWithdraw(address account) public  {
-        uint value = userWithdrawSupply(account);
+    function withdraw() public  {
+        address account = msg.sender;
+        locks[account].balance.update(roundTimestamp(block.timestamp));
+        uint value = locks[account].amount - locks[account].balance.initial.bias;
         if (value > 0) {
-            locks[account].balance -= value;
-            totalLocks.balance -= value;
+            locks[account].amount -= value;
             require(token.transferFrom(address(this), account, value), "failure while transferring");
         }
         //todo Lock delete
     }
 
+    function getStake(uint amount, uint slope, uint cliff) internal returns (uint){
+        return amount;
+    }
 
     function increaseUnlockTime(uint lockId, uint period) public {
 
@@ -124,10 +91,8 @@ contract Staking {
 
     }
 
-
     function roundTimestamp(uint ts) pure internal returns (uint) {
         return ts.div(WEEK).sub(STARTING_POINT_WEEK);
     }
-
 }
 
--- staking/test/contracts/StakingTest.sol
+++ staking/test/contracts/StakingTest.sol
@@ -12,13 +12,14 @@ contract StakingTest {
 
     function _createLock(address staking, address account, uint amount, uint period, uint cliff) external {
         Staking stakingTest = Staking(staking);
-        uint result =  stakingTest.createLock(account, amount, period, cliff);
+        uint result = 2; //
+        stakingTest.createLock(account, amount, period, cliff);
         emit createLockResult(result);
     }
 
-    function _userSypply(address staking, address account) external {
+    function _balanceOf(address staking, address account) external {
         Staking stakingTest = Staking(staking);
-        uint result =  stakingTest.userSypply(account);
+        uint result =  stakingTest.balanceOf(account);
         emit balanceOfResult(result);
     }
 
