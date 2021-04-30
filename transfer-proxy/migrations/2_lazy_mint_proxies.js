47956020c907808641fa733a3933e7ead079c32d
Evgeny Nacu <evgeny.nacu@gmail.com>
2021-04-30 18:23:11 +0300

    deploy lazy mint transfer proxies to e2e

--- transfer-proxy/migrations/2_lazy_mint_proxies.js
+++ transfer-proxy/migrations/2_lazy_mint_proxies.js
@@ -4,11 +4,11 @@ const ERC721LazyMintTransferProxy = artifacts.require('ERC721LazyMintTransferPro
 const ERC1155LazyMintTransferProxy = artifacts.require('ERC1155LazyMintTransferProxy');
 
 module.exports = async function (deployer) {
-	await deployer.deploy(ERC721LazyMintTransferProxy, { gas: 1000000 });
+	await deployer.deploy(ERC721LazyMintTransferProxy, { gas: 1500000 });
 	const erc721Proxy = await ERC721LazyMintTransferProxy.deployed();
-	await erc721Proxy.__OperatorRole_init({ gas: 100000 });
+	await erc721Proxy.__OperatorRole_init({ gas: 200000 });
 
-	await deployer.deploy(ERC1155LazyMintTransferProxy, { gas: 1000000 });
+	await deployer.deploy(ERC1155LazyMintTransferProxy, { gas: 1500000 });
 	const erc1155Proxy = await ERC1155LazyMintTransferProxy.deployed();
-	await erc1155Proxy.__OperatorRole_init({ gas: 100000 });
+	await erc1155Proxy.__OperatorRole_init({ gas: 200000 });
 };
