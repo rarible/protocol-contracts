90c3db71490edf95ced3c002d4201203f66937b3
Evgeny Nacu <evgeny.nacu@gmail.com>
2021-04-30 17:27:55 +0300

    deploy beacons for user proxies

--- tokens/mint1.js
+++ tokens/mint1.js
@@ -1,11 +1,11 @@
 const erc721 = await ERC721Rarible.deployed();
 const uri = "/ipfs/QmWLsBu6nS4ovaHbGAXprD1qEssJu4r5taQfB74sCG51tp";
-const tokenId = "0xfb571F9da71D1aC33E069571bf5c67faDCFf18e4000000000000000000000001";
+const tokenId = "0xfb571F9da71D1aC33E069571bf5c67faDCFf18e4000000000000000000000010";
 const message = [tokenId, uri, [["0xfb571F9da71D1aC33E069571bf5c67faDCFf18e4", 10000]], [], ["0x"]];
 await erc721.mintAndTransfer(message, "0xfb571F9da71D1aC33E069571bf5c67faDCFf18e4", {gas: "500000"});
 
 const erc1155 = await ERC1155Rarible.deployed();
 const uri = "/ipfs/QmWLsBu6nS4ovaHbGAXprD1qEssJu4r5taQfB74sCG51tp";
-const tokenId = "0xfb571F9da71D1aC33E069571bf5c67faDCFf18e4000000000000000000000001";
+const tokenId = "0xfb571F9da71D1aC33E069571bf5c67faDCFf18e4000000000000000000000010";
 const message1 = [tokenId, uri, 10, [["0xfb571F9da71D1aC33E069571bf5c67faDCFf18e4", 10000]], [], ["0x"]];
 await erc1155.mintAndTransfer(message1, "0xfb571F9da71D1aC33E069571bf5c67faDCFf18e4", 10, {gas: "500000"});
