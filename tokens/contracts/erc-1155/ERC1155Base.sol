ff1e26e3aafc4c780e54093c8573bcfd2ec7a2a3
Evgeny Nacu <evgeny.nacu@gmail.com>
2021-04-30 13:51:10 +0300

    revert to ERC721Base, ERC1155Base to make contracts upgradeable

---
+++ tokens/contracts/erc-1155/ERC1155Base.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.6.2 <0.8.0;
+pragma abicoder v2;
+
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC1155/ERC1155BurnableUpgradeable.sol";
+import "./ERC1155DefaultApproval.sol";
+import "./ERC1155Lazy.sol";
+import "../HasContractURI.sol";
+
+abstract contract ERC1155Base is OwnableUpgradeable, ERC1155DefaultApproval, ERC1155BurnableUpgradeable, ERC1155Lazy, HasContractURI {
+
+    string public name;
+    string public symbol;
+
+    function setDefaultApproval(address operator, bool hasApproval) external onlyOwner {
+        _setDefaultApproval(operator, hasApproval);
+    }
+
+    function isApprovedForAll(address _owner, address _operator) public override(ERC1155Upgradeable, ERC1155DefaultApproval, IERC1155Upgradeable) view returns (bool) {
+        return ERC1155DefaultApproval.isApprovedForAll(_owner, _operator);
+    }
+
+    function _mint(address account, uint256 id, uint256 amount, bytes memory data) internal virtual override(ERC1155Upgradeable, ERC1155Lazy) {
+        ERC1155Lazy._mint(account, id, amount, data);
+    }
+
+    function __ERC1155Base_init_unchained(string memory _name, string memory _symbol) internal initializer {
+        name = _name;
+        symbol = _symbol;
+    }
+
+    function uri(uint id) external view override(ERC1155BaseURI, ERC1155Upgradeable) virtual returns (string memory) {
+        return _tokenURI(id);
+    }
+    uint256[50] private __gap;
+}
--- tokens/contracts/erc-721/ERC721Rarible.sol
+++ tokens/contracts/erc-721/ERC721Rarible.sol
@@ -3,27 +3,11 @@
 pragma solidity >=0.6.2 <0.8.0;
 pragma abicoder v2;
 
-import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721BurnableUpgradeable.sol";
-import "./ERC721DefaultApproval.sol";
-import "./ERC721Lazy.sol";
-import "../HasContractURI.sol";
+import "./ERC721Base.sol";
 
-contract ERC721Rarible is OwnableUpgradeable, ERC721DefaultApproval, ERC721BurnableUpgradeable, ERC721Lazy, HasContractURI {
+contract ERC721Rarible is ERC721Base {
 
     event CreateERC721Rarible(address owner, string name, string symbol);
-
-    function setDefaultApproval(address operator, bool hasApproval) external onlyOwner {
-        _setDefaultApproval(operator, hasApproval);
-    }
-
-    function _isApprovedOrOwner(address spender, uint256 tokenId) internal virtual override(ERC721Upgradeable, ERC721DefaultApproval) view returns (bool) {
-        return ERC721DefaultApproval._isApprovedOrOwner(spender, tokenId);
-    }
-
-    function isApprovedForAll(address owner, address operator) public view virtual override(ERC721DefaultApproval, ERC721Upgradeable, IERC721Upgradeable) returns (bool) {
-        return ERC721DefaultApproval.isApprovedForAll(owner, operator);
-    }
 
     function __ERC721Rarible_init(string memory _name, string memory _symbol, string memory baseURI, string memory contractURI) external initializer {
         _setBaseURI(baseURI);
