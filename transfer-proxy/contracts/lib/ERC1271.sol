6af4248c1949e69c104f5d28c72b8d7e61b4986c
Evgeny Nacu <evgeny.nacu@gmail.com>
2021-05-09 08:21:44 +0300

    fix ERC1271 everywhere

--- transfer-proxy/contracts/lib/ERC1271.sol
+++ transfer-proxy/contracts/lib/ERC1271.sol
@@ -5,14 +5,14 @@ pragma solidity >=0.6.9 <0.8.0;
 abstract contract ERC1271 {
 
     // bytes4(keccak256("isValidSignature(bytes,bytes)")
-    bytes4 constant internal MAGICVALUE = 0x20c13b0b;
+    bytes4 constant internal MAGICVALUE = 0x1626ba7e;
 
     /**
      * @dev Should return whether the signature provided is valid for the provided data
      * @param _hash Hash of the data signed on the behalf of address(this)
      * @param _signature Signature byte array associated with _data
      *
-     * MUST return the bytes4 magic value 0x20c13b0b when function passes.
+     * MUST return the bytes4 magic value 0x1626ba7e when function passes.
      * MUST NOT modify state (using STATICCALL for solc < 0.5, view modifier for solc > 0.5)
      * MUST allow external calls
      */
