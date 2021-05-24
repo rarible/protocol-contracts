6567420aa4d6c30b44d054b65667ea573313fd90
Konstantin Sherbakov <sherbakov.konst@yandex.ru>
2021-05-24 10:14:39 +0700

    refactoring(*):delete dubug method

--- staking/contracts/Staking.sol
+++ staking/contracts/Staking.sol
@@ -115,25 +115,6 @@ contract Staking {
         return id;
     }
 
-    function uint2str(uint _i) internal pure returns (string memory _uintAsString) {
-        if (_i == 0) {
-            return "0";
-        }
-        uint j = _i;
-        uint len;
-        while (j != 0) {
-            len++;
-            j /= 10;
-        }
-        bytes memory bstr = new bytes(len);
-        uint k = len - 1;
-        while (_i != 0) {
-            bstr[k--] = byte(uint8(48 + _i % 10));
-            _i /= 10;
-        }
-        return string(bstr);
-    }
-
     function getStake(uint amount, uint slope, uint cliff) internal returns (uint){
         return amount;
     }
