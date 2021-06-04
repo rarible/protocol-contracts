32985c0c242d96f66cd7922a98f98a129eb0c487
Evgeny Nacu <evgeny.nacu@gmail.com>
2021-06-04 18:01:21 +0300

    deployed exchange to ropsten

--- exchange-v2/migrations/2_initial_ExchangeV2.js
+++ exchange-v2/migrations/2_initial_ExchangeV2.js
@@ -9,10 +9,19 @@ const e2e = {
 	royaltiesRegistry: "0xEd9E4775a5d746fd4b36612fD0B2AfcB05b3147C"
 };
 
+const ropsten = {
+	communityWallet: "0xe627243104a101ca59a2c629adbcd63a782e837f",
+	erc20TransferProxy: "0xa5a51d7b4933185da9c932e5375187f661cb0c69",
+	transferProxy: "0xf8e4ecac18b65fd04569ff1f0d561f74effaa206",
+	royaltiesRegistry: "0x1331B6a79101fa18218179e78849f1759b846124"
+};
+
 let settings = {
 	"default": e2e,
 	"e2e": e2e,
-	"e2e-fork": e2e
+	"e2e-fork": e2e,
+	"ropsten": ropsten,
+	"ropsten-fork": ropsten
 };
 
 function getSettings(network) {
@@ -28,7 +37,7 @@ module.exports = async function (deployer, network) {
 
   await deployProxy(
   	ExchangeV2,
-  	[transferProxy, erc20TransferProxy, 100, communityWallet, royaltiesRegistry],
+  	[transferProxy, erc20TransferProxy, 0, communityWallet, royaltiesRegistry],
   	{ deployer, initializer: '__ExchangeV2_init' }
   );
 };
