e892f1d300a08a39e52c1d5bfb82062a6402b536
Evgeny Nacu <evgeny.nacu@gmail.com>
2021-06-12 08:20:37 +0300

    deployed lazy mint tokens

--- tokens/migrations/1_initial_migration.js
+++ tokens/migrations/1_initial_migration.js
@@ -1,5 +1,12 @@
 const Migrations = artifacts.require("Migrations");
 
-module.exports = function (deployer) {
-  deployer.deploy(Migrations, { gas: 200000 });
+module.exports = async function (deployer) {
+	try {
+		const deployed = await Migrations.deployed();
+		if (deployed == null) {
+			await deployer.deploy(Migrations, { gas: 200000 });
+		}
+	} catch(e) {
+		await deployer.deploy(Migrations, { gas: 200000 });
+	}
 };
