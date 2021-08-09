0a2f6ede79921e60c05dd7f13a876fc34507b37a
Konstantin Sherbakov <sherbakov.konst@yandex.ru>
2021-08-09 12:16:09 +0300

    feat(*): correct for success compile

--- tokens/contracts/erc-721/erc-721-meta/ERC721RaribleUserBase.sol
+++ tokens/contracts/erc-721/erc-721-meta/ERC721RaribleUserBase.sol
@@ -26,6 +26,10 @@ abstract contract ERC721RaribleUserBase is OwnableUpgradeable, ERC721BurnableUpg
         }
     }
 
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC165Upgradeable, ERC721Lazy) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+
     function mintAndTransfer(LibERC721LazyMint.Mint721Data memory data, address to) public override virtual {
         require(owner() == data.creators[0].account, "minter is not the owner");
         super.mintAndTransfer(data, to);
