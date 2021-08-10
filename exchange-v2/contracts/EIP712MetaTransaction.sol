f117d9a78fb364a097007e36b81d1b67bb356b02
Konstantin Sherbakov <sherbakov.konst@yandex.ru>
2021-08-10 22:16:43 +0300

    feat(*):add metaTransaction to ExchangeV2

---
+++ exchange-v2/contracts/EIP712MetaTransaction.sol
@@ -0,0 +1,133 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.7.6;
+
+import "@openzeppelin/contracts-upgradeable/utils/ContextUpgradeable.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+
+abstract contract EIP712MetaTransaction is ContextUpgradeable {
+    using SafeMath for uint256;
+
+    bytes32 private constant META_TRANSACTION_TYPEHASH = keccak256(bytes("MetaTransaction(uint256 nonce,address from,bytes functionSignature)"));
+    bytes32 internal constant EIP712_DOMAIN_TYPEHASH = keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)");
+
+    mapping(address => uint256) private nonces;
+    bytes32 internal domainSeparator;
+
+    /*
+     * Meta transaction structure.
+     * No point of including value field here as if user is doing value transfer then he has the funds to pay for gas
+     * He should call the desired function directly in that case.
+     */
+    struct MetaTransaction {
+        uint256 nonce;
+        address from;
+        bytes functionSignature;
+    }
+
+    /*
+     * Domain structure.
+     * Data(information to for making metaTransaction method uniq) about method and contract
+     */
+    struct EIP712Domain {
+        string name;
+        string version;
+        uint256 chainId;
+        address verifyingContract;
+    }
+
+    event MetaTransactionExecuted(address userAddress, address payable relayerAddress, bytes functionSignature);
+
+    function __MetaTransaction_init_unchained(string memory name, string memory version) internal {
+        domainSeparator = keccak256(abi.encode(
+                EIP712_DOMAIN_TYPEHASH,
+                keccak256(bytes(name)),
+                keccak256(bytes(version)),
+                bytes32(getChainID()),
+                address(this)
+            ));
+    }
+
+    function convertBytesToBytes4(bytes memory inBytes) internal returns (bytes4 outBytes4) {
+        if (inBytes.length == 0) {
+            return 0x0;
+        }
+
+        assembly {
+            outBytes4 := mload(add(inBytes, 32))
+        }
+    }
+
+    function executeMetaTransaction(address userAddress,
+        bytes memory functionSignature, bytes32 sigR, bytes32 sigS, uint8 sigV) public payable returns (bytes memory) {
+        bytes4 destinationFunctionSig = convertBytesToBytes4(functionSignature);
+        require(destinationFunctionSig != msg.sig, "functionSignature can not be of executeMetaTransaction method");
+        MetaTransaction memory metaTx = MetaTransaction({
+        nonce : nonces[userAddress],
+        from : userAddress,
+        functionSignature : functionSignature
+        });
+        require(verify(userAddress, metaTx, sigR, sigS, sigV), "Signer and signature do not match");
+        nonces[userAddress] = nonces[userAddress].add(1);
+        // Append userAddress at the end to extract it from calling context
+        (bool success, bytes memory returnData) = address(this).call(abi.encodePacked(functionSignature, userAddress));
+
+        require(success, "Function call not successful");
+        emit MetaTransactionExecuted(userAddress, msg.sender, functionSignature);
+        return returnData;
+    }
+
+    function hashMetaTransaction(MetaTransaction memory metaTx) internal pure returns (bytes32) {
+        return keccak256(abi.encode(
+                META_TRANSACTION_TYPEHASH,
+                metaTx.nonce,
+                metaTx.from,
+                keccak256(metaTx.functionSignature)
+            ));
+    }
+
+    function getNonce(address user) external view returns (uint256 nonce) {
+        nonce = nonces[user];
+    }
+
+    function verify(address user, MetaTransaction memory metaTx, bytes32 sigR, bytes32 sigS, uint8 sigV) internal view returns (bool) {
+        address signer = ecrecover(toTypedMessageHash(hashMetaTransaction(metaTx)), sigV, sigR, sigS);
+        require(signer != address(0), "Invalid signature");
+        return signer == user;
+    }
+
+    function _msgSender() internal view virtual override returns (address payable sender) {
+        if (msg.sender == address(this)) {
+            bytes memory array = msg.data;
+            uint256 index = msg.data.length;
+            assembly {
+            // Load the 32 bytes word from memory with the address on the lower 20 bytes, and mask those.
+                sender := and(mload(add(array, index)), 0xffffffffffffffffffffffffffffffffffffffff)
+            }
+        } else {
+            sender = msg.sender;
+        }
+        return sender;
+    }
+
+    function getChainID() internal pure returns (uint256 id) {
+        assembly {
+            id := chainid()
+        }
+    }
+
+    function getDomainSeparator() private view returns (bytes32) {
+        return domainSeparator;
+    }
+
+    /**
+    * Accept message hash and returns hash message in EIP712 compatible form
+    * So that it can be used to recover signer from signature signed using EIP712 formatted data
+    * https://eips.ethereum.org/EIPS/eip-712
+    * "\\x19" makes the encoding deterministic
+    * "\\x01" is the version byte to make it compatible to EIP-191
+    */
+    function toTypedMessageHash(bytes32 messageHash) internal view returns (bytes32) {
+        return keccak256(abi.encodePacked("\x19\x01", getDomainSeparator(), messageHash));
+    }
+
+}
---
+++ exchange-v2/test/contracts/v2/ExchangeSimpleV2_MetaTx.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.7.6;
+pragma abicoder v2;
+
+import "../../../contracts/ExchangeV2Core.sol";
+import "../../../contracts/EIP712MetaTransaction.sol";
+import "./SimpleTransferManager.sol";
+
+contract ExchangeSimpleV2_MetaTx is ExchangeV2Core, SimpleTransferManager, EIP712MetaTransaction {
+    function __ExchangeSimpleV2_init(
+        INftTransferProxy _transferProxy,
+        IERC20TransferProxy _erc20TransferProxy
+    ) external initializer {
+        __Context_init_unchained();
+        __Ownable_init_unchained();
+        __TransferExecutor_init_unchained(_transferProxy, _erc20TransferProxy);
+        __OrderValidator_init_unchained();
+        __MetaTransaction_init_unchained("ExchangeV2","1");
+    }
+
+    function _msgSender() internal view virtual override(ContextUpgradeable, EIP712MetaTransaction) returns (address payable) {
+        return super._msgSender();
+    }
+}
