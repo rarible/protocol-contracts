90b2fb3b50ca105dfa9d87d564cf6f70cd5aa43f
disenotov <mitri4cool@mail.ru>
2021-08-17 15:17:10 +0300

    fix royaltyProvider migration

--- royalties-registry/migrations/4_royaltProviders.js
+++ royalties-registry/migrations/4_royaltProviders.js
@@ -3,28 +3,20 @@ const RoyaltiesProviderV2Legacy = artifacts.require("RoyaltiesProviderV2Legacy")
 
 const rinkeby = {
     v2Legacy: [],
-    artBlocks: [],
 }
 const mainnet = {
     v2Legacy: [
 
     ],
-    artBlocks: [
-        "0x059edd72cd353df5106d2b9cc5ab83a52287ac3a",
-        "0xa7d8d9ef8d8ce8992df33d8b8cf4aebabd5bd270",
-    ]
 }
 const ropsten = {
     v2Legacy: [],
-    artBlocks: [],
 }
 const e2e = {
     v2Legacy: [],
-    artBlocks: [],
 }
 const def = {
     v2Legacy: [],
-    artBlocks: [],
 }
 
 let settings = {
