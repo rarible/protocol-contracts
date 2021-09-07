720b39a716bdbd57beabcc453a8118469d5c1595
Konstantin Sherbakov <sherbakov.konst@yandex.ru>
2021-09-07 09:18:26 +0300

    refactoring(*): rename test constant: PUNK<->CRYPTO_PUNK

--- custom-matchers/test/AssetPunkMatcher.test.js
+++ custom-matchers/test/AssetPunkMatcher.test.js
@@ -1,6 +1,6 @@
 const AssetMatcherPunk = artifacts.require("AssetMatcherPunk.sol");
 const order = require("../../exchange-v2/test/order.js");
-const { enc, ETH, ERC20, ERC721, ERC1155, PUNK, id } = require("../../exchange-v2/test/assets.js");
+const { enc, ETH, ERC20, ERC721, ERC1155, CRYPTO_PUNK, id } = require("../../exchange-v2/test/assets.js");
 
 contract("AssetMatcherCustom", accounts => {
 	let testingCustom;
@@ -14,8 +14,8 @@ contract("AssetMatcherCustom", accounts => {
       const tokenId = 3000;
       const encodedPunk1 = enc(accounts[5], tokenId);
       const encodedPunk2 = enc(accounts[5], tokenId);
-      const result = await testingPunk.matchAssets(order.AssetType(PUNK, encodedPunk1), order.AssetType(PUNK, encodedPunk2));
-      assert.equal(result[0], PUNK);
+      const result = await testingPunk.matchAssets(order.AssetType(CRYPTO_PUNK, encodedPunk1), order.AssetType(CRYPTO_PUNK, encodedPunk2));
+      assert.equal(result[0], CRYPTO_PUNK);
       assert.equal(result[1], encodedPunk2);
     });
 
@@ -24,7 +24,7 @@ contract("AssetMatcherCustom", accounts => {
       const tokenId2 = 3001;
       const encodedPunk1 = enc(accounts[5], tokenId1);
       const encodedPunk2 = enc(accounts[5], tokenId2);
-      const result = await testingPunk.matchAssets(order.AssetType(PUNK, encodedPunk1), order.AssetType(PUNK, encodedPunk2));
+      const result = await testingPunk.matchAssets(order.AssetType(CRYPTO_PUNK, encodedPunk1), order.AssetType(CRYPTO_PUNK, encodedPunk2));
       assert.equal(result[0], 0);
     });
 
@@ -32,7 +32,7 @@ contract("AssetMatcherCustom", accounts => {
       const tokenId = 3000;
       const encodedPunk1 = enc(accounts[5], tokenId);
       const encodedPunk2 = enc(accounts[6], tokenId);
-      const result = await testingPunk.matchAssets(order.AssetType(PUNK, encodedPunk1), order.AssetType(PUNK, encodedPunk2));
+      const result = await testingPunk.matchAssets(order.AssetType(CRYPTO_PUNK, encodedPunk1), order.AssetType(CRYPTO_PUNK, encodedPunk2));
       assert.equal(result[0], 0);
     });
 
