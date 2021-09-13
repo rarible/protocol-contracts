4c738146d293e0ca2660fff497efb8b56413aecf
Konstantin Sherbakov <sherbakov.konst@yandex.ru>
2021-09-13 09:03:01 +0300

    feat(*):move IAssetMatcher.sol to project interfaces, because custom matchers need interface

--- exchange-v2/contracts/AssetMatcher.sol
+++ exchange-v2/contracts/AssetMatcher.sol
@@ -3,7 +3,7 @@
 pragma solidity 0.7.6;
 pragma abicoder v2;
 
-import "./IAssetMatcher.sol";
+import "../../interfaces/contracts/IAssetMatcher.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
