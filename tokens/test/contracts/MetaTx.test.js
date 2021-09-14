d5e020389fe532f5936a347cd9fe36ba60a1a978
Konstantin Sherbakov <sherbakov.konst@yandex.ru>
2021-09-14 20:07:46 +0300

    test(*):add test which  call metaTransaction in ERC721RaribleMeta token

--- tokens/test/contracts/MetaTx.test.js
+++ tokens/test/contracts/MetaTx.test.js
@@ -9,22 +9,118 @@ const ERC721UserNoMetaTx = artifacts.require("ERC721RaribleUser.sol");
 const ERC1155NoMetaTx = artifacts.require("ERC1155Rarible.sol");
 const ERC1155UserNoMetaTx = artifacts.require("ERC1155RaribleUser.sol");
 
+const web3Abi = require('web3-eth-abi');
+const sigUtil = require('eth-sig-util');
 const truffleAssert = require('truffle-assertions');
 const { expectThrow } = require("@daonomic/tests-common");
 
 let ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
+let publicKey = "0x726cDa2Ac26CeE89F645e55b78167203cAE5410E";
+let privateKey = "0x68619b8adb206de04f676007b2437f99ff6129b672495a6951499c6c56bc2fa6";
+let balanceOfAbi =  {
+  "inputs": [
+    {
+      "internalType": "address",
+      "name": "owner",
+      "type": "address"
+    }
+  ],
+  "name": "balanceOf",
+  "outputs": [
+    {
+      "internalType": "uint256",
+      "name": "",
+      "type": "uint256"
+    }
+  ],
+  "stateMutability": "view",
+  "type": "function"
+};
+const domainType = [{
+    name: "name",
+    type: "string"
+  },
+  {
+    name: "version",
+    type: "string"
+  },
+  {
+    name: "chainId",
+    type: "uint256"
+  },
+  {
+    name: "verifyingContract",
+    type: "address"
+  }
+];
+const metaTransactionType = [{
+    name: "nonce",
+    type: "uint256"
+  },
+  {
+    name: "from",
+    type: "address"
+  },
+  {
+    name: "functionSignature",
+    type: "bytes"
+  }
+];
+let domainData;
+
+const getTransactionData = async (nonce, abi, params) => {
+  const functionSignature = web3Abi.encodeFunctionCall(
+    abi,
+    params
+  );
+
+  let message = {};
+  message.nonce = parseInt(nonce);
+  message.from = publicKey;
+  message.functionSignature = functionSignature;
+  const dataToSign = {
+    types: {
+      EIP712Domain: domainType,
+      MetaTransaction: metaTransactionType
+    },
+    domain: domainData,
+    primaryType: "MetaTransaction",
+    message: message
+  };
+  const signature = sigUtil.signTypedData_v4(new Buffer(privateKey.substring(2, 66), 'hex'), {
+    data: dataToSign
+  });
+//    console.log("test:"+sigUtil.recoverTypedSignature_v4({sig:signature, data:dataToSign}) ); for test only
+  let r = signature.slice(0, 66);
+  let s = "0x".concat(signature.slice(66, 130));
+  let v = "0x".concat(signature.slice(130, 132));
+  v = web3.utils.hexToNumber(v);
+  if (![27, 28].includes(v)) v += 27;
+
+  return {r, s, v, functionSignature};
+}
 
 contract("ERC721MetaTxTest", accounts => {
   let erc721NoMetaTx;
+  let erc721WithMetaTx;
+  let erc721WithMetaTxTest;
   let erc721UserNoMetaTx;
   let erc1155NoMetaTx;
   let erc1155UserNoMetaTx;
 
   beforeEach(async () => {
     erc721NoMetaTx = await deployProxy(ERC721NoMetaTx, ["Rarible", "RARI", "ipfs:/", ""], { initializer: '__ERC721Rarible_init' });
+    erc721WithMetaTx = await deployProxy(ERC721MetaTx, ["Rarible", "RARI", "ipfs:/", ""], { initializer: '__ERC721RaribleMeta_init' });
+
     erc721UserNoMetaTx = await deployProxy(ERC721UserNoMetaTx, ["Rarible", "RARI", "ipfs:/", "", []], { initializer: '__ERC721RaribleUser_init' });
     erc1155NoMetaTx = await deployProxy(ERC1155NoMetaTx, ["Rarible", "RARI", "ipfs:/", ""], { initializer: '__ERC1155Rarible_init' });
     erc1155UserNoMetaTx = await deployProxy(ERC1155UserNoMetaTx, ["Rarible", "RARI", "ipfs:/", "", []], { initializer: '__ERC1155RaribleUser_init' });
+    domainData = {
+          name: "ERC721RaribleMeta",
+          version: "1",
+          verifyingContract: erc721WithMetaTx.address,
+          chainId: 1337
+        };
   });
 
   it("Upgrade, which use MetaTransaction for ERC721RaribleMeta token works", async () => {
@@ -35,6 +131,24 @@ contract("ERC721MetaTxTest", accounts => {
 
   		await upgradeProxy(erc721NoMetaTx.address, ERC721MetaTx);
   		assert.equal(await wrapper.getNonce(ZERO_ADDRESS), 0);
+  });
+
+  it("Use metaTransaction call for ERC721RaribleMeta token", async () => {
+  	let nonce = await erc721WithMetaTx.getNonce(publicKey);
+  	let {
+      r,
+      s,
+      v,
+      functionSignature
+    } = await getTransactionData(nonce, balanceOfAbi, [publicKey]);
+    let resultExecMataTx = await erc721WithMetaTx.executeMetaTransaction(publicKey, functionSignature, r, s, v, {from: accounts[0]});
+    let metaResult;
+    truffleAssert.eventEmitted(resultExecMataTx, 'MetaTransactionExecuted', (ev) => {
+     	metaResult = ev.result;
+      return true;
+    });
+    var newNonce = await erc721WithMetaTx.getNonce(publicKey);
+    assert.isTrue(newNonce.toNumber() == nonce + 1, "Nonce not incremented");
   });
 
   it("Upgrade, which use MetaTransaction for ERC721RaribleUserMeta token works", async () => {
