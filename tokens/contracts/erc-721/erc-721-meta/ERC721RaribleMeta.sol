5f75a82f350e03ab228b011eb657ab3e99ddceba
Konstantin Sherbakov <sherbakov.konst@yandex.ru>
2021-10-01 20:18:32 +0300

    refactoring(*):delete unnecessary base contracts for ERC721RaribleMeta and ERC721RaribleUserMeta

--- tokens/contracts/erc-721/erc-721-meta/ERC721RaribleMeta.sol
+++ tokens/contracts/erc-721/erc-721-meta/ERC721RaribleMeta.sol
@@ -4,14 +4,14 @@ pragma solidity 0.7.6;
 pragma abicoder v2;
 
 import "@rarible/meta-tx/contracts/EIP712MetaTransaction.sol";
-import "./ERC721RaribleBase.sol";
+import "../ERC721Rarible.sol";
 
-contract ERC721RaribleMeta is ERC721RaribleBase, EIP712MetaTransaction {
+contract ERC721RaribleMeta is ERC721Rarible, EIP712MetaTransaction {
 
     event CreateERC721RaribleMeta(address owner, string name, string symbol);
 
     function __ERC721RaribleMeta_init(string memory _name, string memory _symbol, string memory baseURI, string memory contractURI) external initializer {
-        __ERC721RaribleBase_init(_name, _symbol, baseURI, contractURI);
+        initializeERC721Rarible(_name, _symbol, baseURI, contractURI);
         __MetaTransaction_init_unchained("ERC721RaribleMeta", "1");
         emit CreateERC721RaribleMeta(_msgSender(), _name, _symbol);
     }
--- tokens/contracts/erc-721/erc-721-meta/ERC721RaribleUserMeta.sol
+++ tokens/contracts/erc-721/erc-721-meta/ERC721RaribleUserMeta.sol
@@ -4,14 +4,14 @@ pragma solidity 0.7.6;
 pragma abicoder v2;
 
 import "@rarible/meta-tx/contracts/EIP712MetaTransaction.sol";
-import "./ERC721RaribleUserBase.sol";
+import "../ERC721RaribleUser.sol";
 
-contract ERC721RaribleUserMeta is ERC721RaribleUserBase, EIP712MetaTransaction {
+contract ERC721RaribleUserMeta is ERC721RaribleUser, EIP712MetaTransaction {
 
     event CreateERC721RaribleUserMeta(address owner, string name, string symbol);
 
     function __ERC721RaribleUserMeta_init(string memory _name, string memory _symbol, string memory baseURI, string memory contractURI, address[] memory operators) external initializer {
-        __ERC721RaribleUserBase_init(_name, _symbol, baseURI, contractURI, operators);
+        initializeERC721RaribleUser(_name, _symbol, baseURI, contractURI, operators);
         __MetaTransaction_init_unchained("ERC721RaribleUserMeta", "1");
         emit CreateERC721RaribleUserMeta(_msgSender(), _name, _symbol);
     }
