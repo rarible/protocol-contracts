517cd6067e06d1284ea8c45b5d05029a9439d169
disenotov <mitri4cool@mail.ru>
2021-10-08 18:48:22 +0300

    some fixes

--- deploy/migrations/4_royalties_provider_art_blocks.js
+++ deploy/migrations/4_royalties_provider_art_blocks.js
@@ -1,61 +1,10 @@
 const RoyaltiesRegistry = artifacts.require('RoyaltiesRegistry');
 const RoyaltiesProviderArtBlocks = artifacts.require("RoyaltiesProviderArtBlocks");
 
-const ZERO = "0x0000000000000000000000000000000000000000";
-
-const rinkeby = {
-  tokens: [
-    "0x152eeE3DCc5526efd646E9b45c9a9672BfFcc097"
-  ],
-  artblocksAddress: "0xfb571F9da71D1aC33E069571bf5c67faDCFf18e4"
-}
-const mainnet = {
-  tokens: [
-    "0x059edd72cd353df5106d2b9cc5ab83a52287ac3a",
-    "0xa7d8d9ef8d8ce8992df33d8b8cf4aebabd5bd270"
-  ],
-  artblocksAddress: "0x6C093Fe8bc59e1e0cAe2Ec10F0B717D3D182056B"
-}
-const ropsten = {
-  tokens: [],
-  artblocksAddress: ""
-}
-const e2e = {
-  tokens: [],
-  artblocksAddress: ""
-}
-const polygon_mumbai = {
-  tokens: [],
-  artblocksAddress: ""
-}
-const def = {
-  tokens: [],
-  artblocksAddress: ""
-}
-
-let settings = {
-  "default": def,
-  "rinkeby": rinkeby,
-  "rinkeby-fork": rinkeby,
-  "ropsten": ropsten,
-  "ropsten-fork": ropsten,
-  "mainnet": mainnet,
-  "mainnet-fork": mainnet,
-  "e2e": e2e,
-  "polygon_mumbai": polygon_mumbai,
-  "e2e-fork": e2e
-};
-
-function getSettings(network) {
-  if (settings[network] !== undefined) {
-    return settings[network];
-  } else {
-    return settings["default"];
-  }
-}
+const getSettings = require("./config.js")
 
 module.exports = async function (deployer, network) {
-  const settings = getSettings(network)
+  const settings = getSettings(network).artBlocks;
   const royaltiesRegistry = await RoyaltiesRegistry.deployed();
   await setArtBlocksProvider(deployer, network, royaltiesRegistry, settings)
 };
@@ -63,7 +12,7 @@ module.exports = async function (deployer, network) {
 // sets royalties Provider for v2 legacy royalty
 async function setArtBlocksProvider(deployer, network, royaltiesRegistry, settings) {
   // can't deploy without token address
-  if (!settings.tokens || settings.tokens.length == 0) {
+  if (!settings || !settings.tokens || settings.tokens.length == 0) {
     return;
   }
 
