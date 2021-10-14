1b0dd008a106d9ebc91bdb9956a1bdc44f46d522
disenotov <mitri4cool@mail.ru>
2021-10-14 10:09:54 +0300

    fix test

--- transfer-proxy/test/Punk.test.js
+++ transfer-proxy/test/Punk.test.js
@@ -9,30 +9,31 @@ const truffleAssert = require('truffle-assertions');
 /*Proxy  buy punk, sfter Proxy transfer punk to buyer */
 contract("Exchange with PunkTransfer proxies", accounts => {
   let punkIndex = 256;
+  let proxy;
+  const operator = accounts[1]
 
   beforeEach(async () => {
     cryptoPunksMarket = await CryptoPunksMarket.new();
     await cryptoPunksMarket.allInitialOwnersAssigned(); //allow test contract work with Punk CONTRACT_OWNER accounts[0]
+    proxy = await PunkTransferProxy.new();;
+    await proxy.__OperatorRole_init();
+    await proxy.addOperator(operator)
   });
 
 	it("Proxy transfer punk", async () => {
-	  const proxy = await PunkTransferProxy.new();
-
     await cryptoPunksMarket.getPunk(punkIndex, { from: accounts[1] }); //accounts[1] - owner punk with punkIndex
     await cryptoPunksMarket.offerPunkForSaleToAddress(punkIndex, 0, proxy.address, { from: accounts[1] }); //accounts[1] - wants to sell punk with punkIndex, min price 0 wei
 
     assert.equal(await cryptoPunksMarket.balanceOf(accounts[1]), 1); //punk owner - accounts[1]
     const encodedPunkData = await enc(cryptoPunksMarket.address, punkIndex);
 
-    let res = await proxy.transfer(Asset(id("PUNK"), encodedPunkData, 1), accounts[1], accounts[2], { from: accounts[1] });
+    await proxy.transfer(Asset(id("PUNK"), encodedPunkData, 1), accounts[1], accounts[2], { from: operator });
     assert.equal(await cryptoPunksMarket.balanceOf(accounts[1]), 0);
     assert.equal(await cryptoPunksMarket.balanceOf(proxy.address), 0);
     assert.equal(await cryptoPunksMarket.balanceOf(accounts[2]), 1);//punk owner - accounts[2]
   })
 
   it("Try to transfer punk, which not offer to sale, throw", async () => {
-    const proxy = await PunkTransferProxy.new();
-
     await cryptoPunksMarket.getPunk(punkIndex, { from: accounts[1] }); //accounts[1] - owner punk with punkIndex
     await cryptoPunksMarket.offerPunkForSaleToAddress(punkIndex, 0, proxy.address, { from: accounts[1] }); //accounts[1] - wants to sell punk to proxy with punkIndex, min price 0 wei
     let anotherPunkIndex = 300;
@@ -45,8 +46,6 @@ contract("Exchange with PunkTransfer proxies", accounts => {
   })
 
   it("Try to transfer punk, which offer not for proxy.address, throw", async () => {
-		const proxy = await PunkTransferProxy.new();
-
     await cryptoPunksMarket.getPunk(punkIndex, { from: accounts[1] }); //accounts[1] - owner punk with punkIndex
     await cryptoPunksMarket.offerPunkForSaleToAddress(punkIndex, 0, accounts[2], { from: accounts[1] }); //accounts[1] - wants to sell punk to accounts[2]  with punkIndex, min price 0 wei
 
@@ -59,8 +58,6 @@ contract("Exchange with PunkTransfer proxies", accounts => {
   })
 
   it("Check punk event", async () => {
-    const proxy = await PunkTransferProxy.new();
-
     await cryptoPunksMarket.getPunk(punkIndex, { from: accounts[1] }); //accounts[1] - owner punk with punkIndex
     let addressTo;
     let index;
