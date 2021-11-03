b7d3b08a21da9788566bd9b4dbcdd1655175df0a
Dmitry Senotov <15784108+disenotov@users.noreply.github.com>
2021-11-03 12:03:19 +0300

    Rpc 112 make fill (#94)
    * add libOrderDataV2
    
    * RPC-112 make side fill finish
    
    * RPC-112 make side fill finish
    
    * move create2 migration
    
    * fix migration number
    
    * delete V1 constant from LibOrderDataV2
    
    * fix formatting
    
    * fix formatting
    
    * fixx Fill result struct
    
    Co-authored-by: disenotov <mitri4cool@mail.ru>

--- exchange-v2/contracts/ExchangeV2Core.sol
+++ exchange-v2/contracts/ExchangeV2Core.sol
@@ -53,19 +53,13 @@ abstract contract ExchangeV2Core is Initializable, OwnableUpgradeable, AssetMatc
         (LibAsset.AssetType memory makeMatch, LibAsset.AssetType memory takeMatch) = matchAssets(orderLeft, orderRight);
         bytes32 leftOrderKeyHash = LibOrder.hashKey(orderLeft);
         bytes32 rightOrderKeyHash = LibOrder.hashKey(orderRight);
-        uint leftOrderFill = getOrderFill(orderLeft, leftOrderKeyHash);
-        uint rightOrderFill = getOrderFill(orderRight, rightOrderKeyHash);
-        LibFill.FillResult memory newFill = LibFill.fillOrder(orderLeft, orderRight, leftOrderFill, rightOrderFill);
-        require(newFill.takeValue > 0, "nothing to fill");
 
-        if (orderLeft.salt != 0) {
-            fills[leftOrderKeyHash] = leftOrderFill.add(newFill.takeValue);
-        }
-        if (orderRight.salt != 0) {
-            fills[rightOrderKeyHash] = rightOrderFill.add(newFill.makeValue);
-        }
+        LibOrderDataV2.DataV2 memory leftOrderData = LibOrderData.parse(orderLeft);
+        LibOrderDataV2.DataV2 memory rightOrderData = LibOrderData.parse(orderRight);
 
-        (uint totalMakeValue, uint totalTakeValue) = doTransfers(makeMatch, takeMatch, newFill, orderLeft, orderRight);
+        LibFill.FillResult memory newFill = getFillSetNew(orderLeft, orderRight, leftOrderKeyHash, rightOrderKeyHash, leftOrderData, rightOrderData);
+
+        (uint totalMakeValue, uint totalTakeValue) = doTransfers(makeMatch, takeMatch, newFill, orderLeft, orderRight, leftOrderData, rightOrderData);
         if (makeMatch.assetClass == LibAsset.ETH_ASSET_CLASS) {
             require(takeMatch.assetClass != LibAsset.ETH_ASSET_CLASS);
             require(msg.value >= totalMakeValue, "not enough eth");
@@ -78,7 +72,39 @@ abstract contract ExchangeV2Core is Initializable, OwnableUpgradeable, AssetMatc
                 address(msg.sender).transferEth(msg.value.sub(totalTakeValue));
             }
         }
-        emit Match(leftOrderKeyHash, rightOrderKeyHash, orderLeft.maker, orderRight.maker, newFill.takeValue, newFill.makeValue, makeMatch, takeMatch);
+        emit Match(leftOrderKeyHash, rightOrderKeyHash, orderLeft.maker, orderRight.maker, newFill.rightValue, newFill.leftValue, makeMatch, takeMatch);
+    }
+
+    function getFillSetNew(
+        LibOrder.Order memory orderLeft, 
+        LibOrder.Order memory orderRight, 
+        bytes32 leftOrderKeyHash, 
+        bytes32 rightOrderKeyHash,  
+        LibOrderDataV2.DataV2 memory leftOrderData, 
+        LibOrderDataV2.DataV2 memory rightOrderData
+    ) internal returns(LibFill.FillResult memory){
+        uint leftOrderFill = getOrderFill(orderLeft, leftOrderKeyHash);
+        uint rightOrderFill = getOrderFill(orderRight, rightOrderKeyHash);
+        LibFill.FillResult memory newFill = LibFill.fillOrder(orderLeft, orderRight, leftOrderFill, rightOrderFill, leftOrderData.isMakeFill, rightOrderData.isMakeFill);
+        
+        require(newFill.rightValue > 0 && newFill.leftValue > 0, "nothing to fill");
+
+        if (orderLeft.salt != 0) {
+            if (leftOrderData.isMakeFill){
+                fills[leftOrderKeyHash] = leftOrderFill.add(newFill.leftValue);
+            } else {
+                fills[leftOrderKeyHash] = leftOrderFill.add(newFill.rightValue);
+            }
+        }
+        
+        if (orderRight.salt != 0) {
+            if (rightOrderData.isMakeFill){
+                fills[rightOrderKeyHash] = rightOrderFill.add(newFill.rightValue);
+            } else {
+                fills[rightOrderKeyHash] = rightOrderFill.add(newFill.leftValue);
+            }
+        }
+        return newFill;
     }
 
     function getOrderFill(LibOrder.Order memory order, bytes32 hash) internal view returns (uint fill) {
