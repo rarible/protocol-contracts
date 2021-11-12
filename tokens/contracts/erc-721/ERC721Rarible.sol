0c005f247f00b56a4591515af5b1faf90d8db7b0
disenotov <mitri4cool@mail.ru>
2021-11-12 18:54:08 +0300

    deploy new factories and minimal ERC721 on rinkeby

--- tokens/contracts/erc-721/ERC721Rarible.sol
+++ tokens/contracts/erc-721/ERC721Rarible.sol
@@ -6,29 +6,11 @@ pragma abicoder v2;
 import "./ERC721Base.sol";
 
 contract ERC721Rarible is ERC721Base {
-    /// @dev true if collection is private, false if public
-    bool isPrivate;
 
     event CreateERC721Rarible(address owner, string name, string symbol);
-    event CreateERC721RaribleUser(address owner, string name, string symbol);
-
-    function __ERC721RaribleUser_init(string memory _name, string memory _symbol, string memory baseURI, string memory contractURI, address[] memory operators, address transferProxy, address lazyTransferProxy) external initializer {
-        __ERC721Rarible_init_unchained(_name, _symbol, baseURI, contractURI, transferProxy, lazyTransferProxy);
-
-        for(uint i = 0; i < operators.length; i++) {
-            setApprovalForAll(operators[i], true);
-        }
-
-        isPrivate = true;
-        emit CreateERC721RaribleUser(_msgSender(), _name, _symbol);
-    }
 
     function __ERC721Rarible_init(string memory _name, string memory _symbol, string memory baseURI, string memory contractURI, address transferProxy, address lazyTransferProxy) external initializer {
         __ERC721Rarible_init_unchained(_name, _symbol, baseURI, contractURI, transferProxy, lazyTransferProxy);
-
-        
-
-        isPrivate = false;
         emit CreateERC721Rarible(_msgSender(), _name, _symbol);
     }
 
@@ -49,12 +31,5 @@ contract ERC721Rarible is ERC721Base {
         _setDefaultApproval(lazyTransferProxy, true);
     }
 
-    function mintAndTransfer(LibERC721LazyMint.Mint721Data memory data, address to) public override virtual {
-        if (isPrivate){
-            require(owner() == data.creators[0].account, "minter is not the owner");
-        }
-        super.mintAndTransfer(data, to);
-    }
-
-    uint256[49] private __gap;
+    uint256[50] private __gap;
 }
