d92c94157dd62dcc9c1196377ba1711ad8e7d664
disenotov <mitri4cool@mail.ru>
2021-11-16 18:41:23 +0300

    fix migrations

--- deploy/migrations/12_deploy_ERC721Minimal_tokens_factory.js
+++ deploy/migrations/12_deploy_ERC721Minimal_tokens_factory.js
@@ -16,7 +16,10 @@ const TransferProxy = artifacts.require('TransferProxy');
 const ERC721LazyMintTransferProxy = artifacts.require('ERC721LazyMintTransferProxy');
 const ERC1155LazyMintTransferProxy = artifacts.require('ERC1155LazyMintTransferProxy');
 
-async function deployMinimal721(deployer, network, transferProxy, erc721LazyMintTransferProxy) {
+module.exports = async function (deployer, network) {
+  const transferProxy = (await TransferProxy.deployed()).address;
+  const erc721LazyMintTransferProxy = (await ERC721LazyMintTransferProxy.deployed()).address;
+
   //deploying erc721 minimal
   const erc721Proxy = await deployProxy(ERC721RaribleMinimal, ["Rarible", "RARI", "ipfs:/", "", transferProxy, erc721LazyMintTransferProxy], { deployer, initializer: '__ERC721Rarible_init' });
   console.log("deployed erc721 minimal at", erc721Proxy.address)
@@ -32,14 +35,5 @@ async function deployMinimal721(deployer, network, transferProxy, erc721LazyMint
   //deploying factory
   const factory721 = await deployer.deploy(ERC721RaribleFactoryC2, beacon721.address, transferProxy, erc721LazyMintTransferProxy, { gas: 2500000 });
   console.log(`deployed factory721 minimal at ${factory721.address}`)
-}
-
-module.exports = async function (deployer, network) {
-  const transferProxy = (await TransferProxy.deployed()).address;
-  const erc721LazyMintTransferProxy = (await ERC721LazyMintTransferProxy.deployed()).address;
-  const erc1155LazyMintTransferProxy = (await ERC1155LazyMintTransferProxy.deployed()).address;
-
-  //deploy new erc721Minimal proxy and factory, use old 721 beacon
-  await deployMinimal721(deployer, network, transferProxy, erc721LazyMintTransferProxy);
   
 };
