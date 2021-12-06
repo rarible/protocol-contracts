1e8f3235e6d82a7f93fc5e37564e76bea73a3593
Hadrienlc <hadrienlc@gmail.com>
2021-12-06 16:56:06 +0700

    set MinterAccessControl abstract

--- tokens/contracts/access/MinterAccessControl.sol
+++ tokens/contracts/access/MinterAccessControl.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts-upgradeable/proxy/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/EnumerableSetUpgradeable.sol";
 
-contract MinterAccessControl is Initializable, OwnableUpgradeable {
+abstract contract MinterAccessControl is Initializable, OwnableUpgradeable {
     using EnumerableSetUpgradeable for EnumerableSetUpgradeable.AddressSet;
 
     EnumerableSetUpgradeable.AddressSet private _minters;
