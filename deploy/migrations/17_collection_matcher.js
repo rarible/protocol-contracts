a5c379309b955f5d21fc00bbc734721e414bbd24
disenotov <mitri4cool@mail.ru>
2021-12-28 17:30:43 +0300

    deploy collection asset matcher on rinkeby, ropsten

--- deploy/migrations/17_collection_matcher.js
+++ deploy/migrations/17_collection_matcher.js
@@ -5,10 +5,12 @@ const ExchangeV2 = artifacts.require('ExchangeV2');
 const { id } = require("./config.js");
 
 module.exports = async function (deployer) {
-  const ex = await ExchangeV2.deployed();
+  //deploy asset matcher for collections
   await deployer.deploy(AssetMatcherCollection, { gas: 1000000 });
   const matcher = await AssetMatcherCollection.deployed();
-  await matcher.__AssetMatcherCollection_init();
-  await matcher.addOperator(ex.address);
+  console.log("asset matcher for collections deployed at", matcher.address)
+
+  // set it in ExchangeV2
+  const ex = await ExchangeV2.deployed();
   await ex.setAssetMatcher(id("COLLECTION"), matcher.address);
 };
