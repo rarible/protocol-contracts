88e3ca654ab4b6c00a10c8de77ff61998bfee33e
Konstantin Sherbakov <sherbakov.konst@yandex.ru>
2022-01-14 17:43:24 +0700

    fix(*): fix function signature SimpleTransferManager

--- exchange-v2/test/contracts/v2/SimpleTransferManager.sol
+++ exchange-v2/test/contracts/v2/SimpleTransferManager.sol
@@ -13,7 +13,8 @@ abstract contract SimpleTransferManager is ITransferManager {
         LibOrder.Order memory leftOrder,
         LibOrder.Order memory rightOrder,
         LibOrderDataV2.DataV2 memory leftOrderData,
-        LibOrderDataV2.DataV2 memory rightOrderData
+        LibOrderDataV2.DataV2 memory rightOrderData,
+        LibFee.MatchFees memory matchFees
     ) override internal returns (uint totalMakeValue, uint totalTakeValue) {
         address leftOrderBeneficiary = leftOrder.maker;
         address rightOrderBeneficiary = rightOrder.maker;
