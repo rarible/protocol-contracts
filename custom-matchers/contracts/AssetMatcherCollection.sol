ff5041f735c2f851ef720ee8b684d9705b768199
Eugene Nacu <evgeny.nacu@gmail.com>
2022-01-21 16:45:21 +0300

    fix: tests

--- custom-matchers/contracts/AssetMatcherCollection.sol
+++ custom-matchers/contracts/AssetMatcherCollection.sol
@@ -6,20 +6,15 @@ pragma abicoder v2;
 import "@rarible/exchange-interfaces/contracts/IAssetMatcher.sol";
 import "@rarible/lazy-mint/contracts/erc-721/LibERC721LazyMint.sol";
 import "@rarible/lazy-mint/contracts/erc-1155/LibERC1155LazyMint.sol";
-import "@rarible/transfer-proxy/contracts/roles/OperatorRole.sol";
 
 /*
  * Custom matcher for collection (assetClass, that need any/all elements from collection)
  */
-contract AssetMatcherCollection is IAssetMatcher, Initializable, OperatorRole {
+contract AssetMatcherCollection is IAssetMatcher {
 
     bytes constant EMPTY = "";
 
-    function __AssetMatcherCollection_init() initializer external {
-        __Ownable_init();
-    }
-
-    function matchAssets(LibAsset.AssetType memory leftAssetType, LibAsset.AssetType memory rightAssetType) onlyOperator public view override returns (LibAsset.AssetType memory) {
+    function matchAssets(LibAsset.AssetType memory leftAssetType, LibAsset.AssetType memory rightAssetType) public view override returns (LibAsset.AssetType memory) {
         if (
             (rightAssetType.assetClass == LibAsset.ERC721_ASSET_CLASS) || 
             (rightAssetType.assetClass == LibERC721LazyMint.ERC721_LAZY_ASSET_CLASS) ||
