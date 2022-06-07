9d4f9bdf30405cf9703a660664c57d2c9c6624bf
disenotov <mitri4cool@mail.ru>
2022-06-07 13:55:42 +0300

    feat: BRAVO-2306 fix royalties 2981 rounding

--- royalties/contracts/LibRoyalties2981.sol
+++ royalties/contracts/LibRoyalties2981.sol
@@ -11,13 +11,17 @@ library LibRoyalties2981 {
     bytes4 constant _INTERFACE_ID_ROYALTIES = 0x2a55205a;
     uint96 constant _WEIGHT_VALUE = 1000000;
 
+    //1 = 0,01% = 100
+
+    //5 000 = 50% = 500 000
+
     /*Method for converting amount to percent and forming LibPart*/
     function calculateRoyalties(address to, uint256 amount) internal view returns (LibPart.Part[] memory) {
         LibPart.Part[] memory result;
         if (amount == 0) {
             return result;
         }
-        uint256 percent = (amount * 100 / _WEIGHT_VALUE) * 100;
+        uint256 percent = amount * 10000 / _WEIGHT_VALUE;
         require(percent < 10000, "Royalties 2981, than 100%");
         result = new LibPart.Part[](1);
         result[0].account = payable(to);
