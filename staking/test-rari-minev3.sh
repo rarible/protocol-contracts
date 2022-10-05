b27f8926facf63cffe37e41882e20e3523ace326
Vadim Fadeev <v.fadeev@rarible.com>
2022-10-05 10:51:33 +0100

    feat(*): add test libs

---
+++ staking/test-rari-minev3.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+truffle test ./test/RariMineV3.test.js ./contracts/Staking.sol ./test/contracts/TestERC20.sol ./contracts/RariMineV3.sol ./contracts/IRariMine.sol ./test/contracts/TestStaking.sol ./test/contracts/LibSignatureTest.sol ./test/contracts/LibEncoderTest.sol
---
+++ staking/test/contracts/LibSignatureTest.sol
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.7.6;
+
+import "@rarible/lib-signature/test/contracts/LibSignatureTest.sol";
