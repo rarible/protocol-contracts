4d81cacaaa5c7b615a92e52f37807384885b1b25
Vadim Fadeev <v.fadeev@velvetech.ru>
2022-10-10 12:43:47 +0100

    fix(*): fix run executor tests

--- transfer-manager/test-transfer-executor.sh
+++ transfer-manager/test-transfer-executor.sh
@@ -6,4 +6,5 @@ truffle test \
         ./test/contracts/ERC20TransferProxyTest.sol \
         ./test/contracts/TestERC20.sol \
         ./test/contracts/TestERC721.sol \
-        ./test/contracts/TestERC1155.sol
+        ./test/contracts/TestERC1155.sol \
+        ./test/contracts/TestERC20ZRX.sol
