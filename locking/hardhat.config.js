cf3478d1ab13cb7705dba6422c99cf335e026de4
Vadim Fadeev <v.fadeev@velvetech.ru>
2023-01-13 14:02:54 +0000

    feat(*): add hardhat to locking

--- locking/hardhat.config.js
+++ locking/hardhat.config.js
@@ -2,5 +2,27 @@
 require("@nomiclabs/hardhat-truffle5");
 
 module.exports = {
-  solidity: "0.7.6",
-};
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+    }
+  },
+  solidity: {
+    version: "0.7.6",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+  mocha: {
+    timeout: 40000
+  }
+}
