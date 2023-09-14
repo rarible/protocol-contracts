d207e318e0545d00cdc80aabe254188d6b9b44e3
disenotov <mitri4cool@mail.ru>
2023-09-15 00:14:02 +0300

    feat: PT-3744 artblocksV2 royalties provider

--- deploy/contracts/RoyaltiesRegistryImport.sol
+++ deploy/contracts/RoyaltiesRegistryImport.sol
@@ -6,3 +6,4 @@ pragma abicoder v2;
 import {RoyaltiesRegistry} from "@rarible/royalties-registry/contracts/RoyaltiesRegistry.sol";
 import {RoyaltiesProviderV2Legacy} from "@rarible/royalties-registry/contracts/providers/RoyaltiesProviderV2Legacy.sol";
 import {RoyaltiesProviderArtBlocks} from "@rarible/royalties-registry/contracts/providers/RoyaltiesProviderArtBlocks.sol";
+import {RoyaltiesProviderArtBlocksV2} from "@rarible/royalties-registry/contracts/providers/RoyaltiesProviderArtBlocksV2.sol";
