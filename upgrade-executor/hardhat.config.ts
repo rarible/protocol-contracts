89d78798fd54791fcd76638ce4a61fa2bd7684ca
Vadim Fadeev <xboxfadeev@gmail.com>
2023-11-28 09:43:21 +0000

    feat: add run scripts

--- upgrade-executor/hardhat.config.ts
+++ upgrade-executor/hardhat.config.ts
@@ -5,6 +5,7 @@ import "@nomicfoundation/hardhat-toolbox";
 import "hardhat-deploy";
 import "@openzeppelin/hardhat-upgrades";
 import "@nomiclabs/hardhat-truffle5";
+import "./tasks";
 
 import type {
   HttpNetworkUserConfig,
