ddd3a8be527506555e2099a0de3de993b247871b
disenotov <mitri4cool@mail.ru>
2023-12-06 12:55:20 +0200

    chore: fixed formatting

--- upgrade-executor/tasks/transfer-ownership.ts
+++ upgrade-executor/tasks/transfer-ownership.ts
@@ -22,21 +22,21 @@ export async function transferOwnership(
 
     // Transfer ownership of each access contract
     for (const [key, address] of Object.entries(settings.accessControl)) {
-        console.log(`Add admin role of ${key} to ${newOwner}`);
-        const contract = AccessControlUpgradeable__factory.connect(address, signer);
-        const TIMELOCK_ADMIN_ROLE = ethers.utils.id("TIMELOCK_ADMIN_ROLE");
-        const tx = await contract.grantRole(TIMELOCK_ADMIN_ROLE, newOwner)
-        console.log(`Add admin role: wait 5 confirmations`);
-        await tx.wait(5)
-        console.log(`Add admin role of ${key} successfully`);
+      console.log(`Add admin role of ${key} to ${newOwner}`);
+      const contract = AccessControlUpgradeable__factory.connect(address, signer);
+      const TIMELOCK_ADMIN_ROLE = ethers.utils.id("TIMELOCK_ADMIN_ROLE");
+      const tx = await contract.grantRole(TIMELOCK_ADMIN_ROLE, newOwner)
+      console.log(`Add admin role: wait 5 confirmations`);
+      await tx.wait(5)
+      console.log(`Add admin role of ${key} successfully`);
 
-        const oldOwner = await signer.getAddress();
+      const oldOwner = await signer.getAddress();
 
-        //now renouncing admin role
-        const tx2 = await contract.renounceRole(TIMELOCK_ADMIN_ROLE, oldOwner)
-        console.log(`Renounce admin role: wait 5 confirmations`);
-        await tx2.wait(5)
-        console.log(`Renounce admin role of ${key} successfully`);
+      //now renouncing admin role
+      const tx2 = await contract.renounceRole(TIMELOCK_ADMIN_ROLE, oldOwner)
+      console.log(`Renounce admin role: wait 5 confirmations`);
+      await tx2.wait(5)
+      console.log(`Renounce admin role of ${key} successfully`);
     }
 
     console.log("Ownership transfer complete.");
