caf3bd36c8baa232c93090392e86d48cfe5533db
disenotov <mitri4cool@mail.ru>
2023-12-19 18:43:43 +0200

    chore: revert not needed fix

--- hardhat-deploy/tasks/import-truffle-deployment.ts
+++ hardhat-deploy/tasks/import-truffle-deployment.ts
@@ -6,10 +6,6 @@ import { DeploymentsExtension } from 'hardhat-deploy/types';
 import "hardhat-deploy";
 import { Console } from 'console';
 
-/*
-npx hardhat import-truffle-deployments --network polygon_dev --artifacts ../deploy/build/contracts 
-*/
-const adminProxyAddress = "0x83fEAb44931FC867273D2EebCc75965EbE50134A"
 
 task(
   'import-truffle-deployments',
@@ -25,49 +21,19 @@ task(
       const { log, save, getExtendedArtifact } = deployments as DeploymentsExtension;
 
       const chainId = network.config.chainId!;
-      console.log(chainId)
       const ARTIFACTS_PATH = path.resolve(taskArgs.artifacts);
       const artifacts = await fs.readdir(ARTIFACTS_PATH);
       console.log(JSON.stringify(artifacts));
-      
-      const ignoreList = [
-        //not in this repo
-        "CryptoPunksMarket", 
-        "ExchangeOrdersHolderV1", 
-        "ExchangeStateV1", 
-        "ExchangeV1", 
-        "Locking", 
-        "TransferProxyForDeprecated", 
-        "Migrations",
-
-        //not needed
-        "AuctionHouse1155",
-        "AuctionHouse721",
-        "PunkTransferProxy",
-        "TestERC20",
-        "Wrapper",
-
-        //proxy
-        "RoyaltiesRegistry",
-        "ExchangeV2",
-        "ERC1155Rarible",
-        "ERC721RaribleMinimal",
-        "ERC721Rarible",
-        "ERC721RaribleMeta",
-        "ERC1155RaribleMeta",
-        "ExchangeMetaV2",
-      ];
-      
+      const ignoreList = ["CryptoPunksMarket", "ExchangeOrdersHolderV1", "ExchangeStateV1", "ExchangeV1", "Locking", "TransferProxyForDeprecated"];
       const truffleDeployedAddresses = await Promise.all(
         artifacts.map(async (artifactPath) => {
           const file = await import(ARTIFACTS_PATH + '/' + artifactPath);
           const address = file.networks[chainId]?.address;
+          console.log( address);
           if (address && !ignoreList.includes(file.contractName)) return [file.contractName, address];
           else return [];
         })
       ).then((result) => result.filter((p) => p.length !== 0));
-
-      console.log(truffleDeployedAddresses)
 
       await Promise.all(
         truffleDeployedAddresses.map(async ([name, address]) => {
@@ -80,45 +46,6 @@ task(
       );
 
       console.log(`Done! Exported ${truffleDeployedAddresses.length} deployed contracts`);
-
-      const proxy = [ 
-        "RoyaltiesRegistry",
-        "ExchangeV2",
-        "ERC1155Rarible",
-        "ERC721RaribleMinimal",
-        "ERC721Rarible",
-        "ERC721RaribleMeta",
-        "ERC1155RaribleMeta",
-        "ExchangeMetaV2",
-      ]
-
-      const proxyAddresses = await Promise.all(
-        artifacts.map(async (artifactPath) => {
-          const file = await import(ARTIFACTS_PATH + '/' + artifactPath);
-          const address = file.networks[chainId]?.address;
-          if (address && proxy.includes(file.contractName)) return [file.contractName, address];
-          else return [];
-        })
-      ).then((result) => result.filter((p) => p.length !== 0));
-
-      //console.log(proxyAddresses) 
-      
-      console.log()
-      console.log("proxies:")
-      console.log()
-      await save("DefaultProxyAdmin", {
-        address: adminProxyAddress,
-        ...(await getExtendedArtifact("ProxyAdmin"))
-      });
-
-      for (const [contractName, address] of proxyAddresses) {
-        await save(contractName + "_Proxy", {
-          address: address,
-          ...(await getExtendedArtifact("TransparentUpgradeableProxy"))
-        });
-        console.log(contractName,"==============", address)
-        console.log()
-      }
     }
   );
 
