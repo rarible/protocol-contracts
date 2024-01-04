3ff8d0a4e0c7af6ca0b90450e8c3fbe5b3d4808d
disenotov <mitri4cool@mail.ru>
2024-01-04 14:27:02 +0200

    feat: PT-4448 prepare exchangeV2 update migration, update exchangeV2 on goerli, polygon_mumbai

---
+++ hardhat-deploy/deploy/101_update_exchangeV2.ts
@@ -0,0 +1,34 @@
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import { DeployFunction } from 'hardhat-deploy/types';
+
+import { getConfig } from '../utils/utils'
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deploy } = hre.deployments;
+  const { deployer } = await hre.getNamedAccounts();
+  const { deploy_meta, deploy_non_meta } = getConfig(hre.network.name);
+
+  let contractName: string = "";
+  //deploying ExchangeV2 with meta support if needed
+  if (!!deploy_meta) {
+    contractName = "ExchangeMetaV2";
+  }
+
+  if (!!deploy_non_meta) {
+    contractName = "ExchangeV2";
+  }
+
+  // deploy ExchangeV2 and initialise contract
+  await deploy(contractName, {
+    from: deployer,
+    proxy: {
+      proxyContract: "OpenZeppelinTransparentProxy",
+    },
+    log: true,
+    autoMine: true,
+  });
+
+};
+
+export default func;
+func.tags = ['update-exchange'];
