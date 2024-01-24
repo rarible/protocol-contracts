3da3fdbae0b186b2845ebc7c2e0d14c94d0070b3
Vadim Fadeev <xboxfadeev@gmail.com>
2024-01-24 10:06:13 +0000

    feat: add verify

--- projects/tokens/scripts/zk-deploy-721-factory.ts
+++ projects/tokens/scripts/zk-deploy-721-factory.ts
@@ -1,11 +1,16 @@
 import hre from "hardhat";
 import { zksyncEthers } from "hardhat";
 
+
+// "0x9301194711E0015316AA0d9e0F184dFB87f70582",
+// "0x85a26E6D52239817570Ff643bA09E3AA5393A805",
+// "0x11983886da3c379E507A874649C96D7EEd086c32"
+
 async function main() {
   console.log("zk-deploy-721-factory.ts");
   const factoryFactory721 = await hre.zksyncEthers.getContractFactory("ERC721RaribleFactoryC2");
   console.log("start deploy")
-  const factory721 = await factoryFactory721.deploy("0xb36603d6956bC855f5F246DFDb77979a2EB63b53", "0x85a26E6D52239817570Ff643bA09E3AA5393A805", "0x11983886da3c379E507A874649C96D7EEd086c32");
+  const factory721 = await factoryFactory721.deploy("0x9301194711E0015316AA0d9e0F184dFB87f70582", "0x85a26E6D52239817570Ff643bA09E3AA5393A805", "0x11983886da3c379E507A874649C96D7EEd086c32");
 
   await factory721.deployed();
 
--- projects/tokens/scripts/zk-get-address-collection.ts
+++ projects/tokens/scripts/zk-get-address-collection.ts
@@ -1,1 +1,34 @@
-// factory at 0x4cA92075aB425A1AB753C731730A893AA32A0E08
+// factory at 0x5F577Dd877b0136ab7ed8A4d7b303aD38348a1Ea
+
+// "0x9301194711E0015316AA0d9e0F184dFB87f70582",
+// "0x85a26E6D52239817570Ff643bA09E3AA5393A805",
+// "0x11983886da3c379E507A874649C96D7EEd086c32"
+
+import { ERC721RaribleFactoryC2 } from "../typechain-types"
+
+import hre from "hardhat";
+
+// https://sepolia.explorer.zksync.io/tx/0xadb7f5955bb0f87fe2de2fa777a0d03ded56c3a11ffdca7e6f47883f89a5d47e#eventlog
+// taget address 0xff0e7865e797bac2fe9cf30996abcd9ddc8de84c
+
+async function main() {
+  console.log("zk-get-address-collection.ts");
+  const factoryFactory721 = await hre.zksyncEthers.getContractFactory("ERC721RaribleFactoryC2") as ERC721RaribleFactoryC2;
+  console.log("start deploy")
+  const factory721 = factoryFactory721.attach("0x5F577Dd877b0136ab7ed8A4d7b303aD38348a1Ea");
+  const targetAddress = await factory721["getAddress(string,string,string,string,uint256)"]("test", "test", "test", "test", 1000);
+
+  console.log("factory721 deployed to:", factory721.address);
+
+  console.log("factory721 cacl collection address:", targetAddress);
+  console.log("factory721 true collection address:", "0xff0e7865e797bac2fe9cf30996abcd9ddc8de84c");
+  console.log(factory721.address);
+}
+
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
