b5c6604f70af1f927564793e78766dfcf4a9da1f
Vadim Fadeev <xboxfadeev@gmail.com>
2024-05-03 13:43:03 +0100

    feat: refactoring

---
+++ projects/hardhat-deploy/deploy/000_deploy_DefaultProxyAdmin.ts
@@ -0,0 +1,27 @@
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import { DeployFunction } from 'hardhat-deploy/types';
+import { getOwner } from './utils';
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+
+  console.log(`deploying contracts on network ${hre.network.name}`)
+
+  const { deploy } = hre.deployments;
+  const { deployer } = await hre.getNamedAccounts();
+  const owner  = await getOwner(hre);
+  console.log("deploying contracts with the account:", deployer);
+  if(deployer != owner) {
+    await deploy('DefaultProxyAdmin', {
+      from: deployer,
+      args: [owner],
+      log: true,
+      autoMine: true,
+      deterministicDeployment: process.env.DETERMENISTIC_DEPLOYMENT_SALT,
+      skipIfAlreadyDeployed: process.env.SKIP_IF_ALREADY_DEPLOYED ? true: false,
+    });
+  }
+
+
+};
+export default func;
+func.tags = ['all', "000"];
