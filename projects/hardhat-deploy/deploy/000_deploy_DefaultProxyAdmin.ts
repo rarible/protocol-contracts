68d7f5b0d01b39cb428f1a8a7b46b5cf0e3023e3
Vadim Fadeev <xboxfadeev@gmail.com>
2024-05-03 12:12:53 +0100

    feat: add deployment proxy admin

--- projects/hardhat-deploy/deploy/000_deploy_DefaultProxyAdmin.ts
+++ projects/hardhat-deploy/deploy/000_deploy_DefaultProxyAdmin.ts
@@ -1,14 +1,6 @@
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import { DeployFunction } from 'hardhat-deploy/types';
 import { getOwner } from './utils';
-import { Ownable } from '../typechain-types';
-
-
-async function getDefaultProxyAdmin(hre: HardhatRuntimeEnvironment): Promise<Ownable> {
-  const contractName = "DefaultProxyAdmin";
-  const address = (await hre.deployments.get(contractName)).address;
-  return (await hre.ethers.getContractAt(contractName, address)) as Ownable
-}
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 
@@ -19,9 +11,15 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const owner  = await getOwner(hre);
   console.log("deploying contracts with the account:", deployer);
 
-  const defaultProxyAdmin = await getDefaultProxyAdmin(hre);
-  console.log("defaultProxyAdmin owner:", defaultProxyAdmin.owner);
+  await deploy('DefaultProxyAdmin', {
+    from: deployer,
+    args: [owner],
+    log: true,
+    autoMine: true,
+    deterministicDeployment: process.env.DETERMENISTIC_DEPLOYMENT_SALT,
+    skipIfAlreadyDeployed: process.env.SKIP_IF_ALREADY_DEPLOYED ? true: false,
+  });
 
 };
 export default func;
-func.tags = ['all', 'transfer-owner-admin', "906"];
+func.tags = ['all', "000"];
