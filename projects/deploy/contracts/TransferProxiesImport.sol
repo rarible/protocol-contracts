b5110cc7a0b712ce16339756e6e3f190e4a8d443
Eugene Nacu <evgeny.nacu@gmail.com>
2024-06-19 14:50:50 +0100

    fix: removed uneeded code

--- projects/deploy/contracts/TransferProxiesImport.sol
+++ projects/deploy/contracts/TransferProxiesImport.sol
@@ -6,4 +6,3 @@ import {ERC721LazyMintTransferProxy} from "@rarible/transfer-proxy/contracts/laz
 import {ERC1155LazyMintTransferProxy} from "@rarible/transfer-proxy/contracts/lazy-mint/erc1155/ERC1155LazyMintTransferProxy.sol";
 import {TransferProxy} from "@rarible/transfer-proxy/contracts/proxy/TransferProxy.sol";
 import {ERC20TransferProxy} from "@rarible/transfer-proxy/contracts/proxy/ERC20TransferProxy.sol";
-import {PunkTransferProxy} from "@rarible/transfer-proxy/contracts/punk/PunkTransferProxy.sol";
