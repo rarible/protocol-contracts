67f83abff529f93498dd682716022675a7af7e2e
Eugene Nacu <evgeny.nacu@gmail.com>
2024-08-07 23:38:47 +0100

    fix: extracted RariFeesDrop, improved tests

--- projects/drop/contracts/OpenEditionRariFees.sol
+++ projects/drop/contracts/OpenEditionRariFees.sol
@@ -29,25 +29,23 @@ import "@thirdweb-dev/contracts/lib/CurrencyTransferLib.sol";
 import "@thirdweb-dev/contracts/extension/Multicall.sol";
 import "@thirdweb-dev/contracts/extension/ContractMetadata.sol";
 import "@thirdweb-dev/contracts/extension/Royalty.sol";
-import "@thirdweb-dev/contracts/extension/PrimarySale.sol";
 import "@thirdweb-dev/contracts/extension/Ownable.sol";
 import "@thirdweb-dev/contracts/extension/SharedMetadata.sol";
 import "@thirdweb-dev/contracts/extension/PermissionsEnumerable.sol";
 import "@thirdweb-dev/contracts/extension/Drop.sol";
-import "./CustomFlatPlatformFee.sol";
+import "./RariFeesDrop.sol";
 
-contract OpenEditionCustomFee is
+contract OpenEditionRariFees is
     Initializable,
     ContractMetadata,
     Royalty,
-    PrimarySale,
     Ownable,
     SharedMetadata,
     PermissionsEnumerable,
-    Drop,
     ERC2771ContextUpgradeable,
     Multicall,
-    ERC721AQueryableUpgradeable
+    ERC721AQueryableUpgradeable,
+    RariFeesDrop
 {
     using StringsUpgradeable for uint256;
 
@@ -62,35 +60,6 @@ contract OpenEditionCustomFee is
 
     /// @dev Max bps in the thirdweb system.
     uint256 private constant MAX_BPS = 10_000;
-
-    struct FeeRecipient {
-        address recipient;
-        uint96 value;
-    }
-
-    struct Fees {
-        uint56 protocolFee; //todo read this from static contract
-        address protocolFeeRecipient; //todo read this from static contract
-        uint256 creatorFinderFee;
-        FeeRecipient creatorFinderFeeRecipient1;
-        FeeRecipient creatorFinderFeeRecipient2;
-        uint256 buyerFinderFee;
-    }
-
-    /// @dev Protocol fee, amount
-    uint256 private protocolFee;
-    /// @dev Protocol fee recipient
-    address private protocolFeeRecipient;
-
-    /// @dev Creator finder fee, amount
-    uint256 private creatorFinderFee;
-    /// @dev Creator finder fee recipient 1. If value == 10000 then recipient 2 is not read
-    FeeRecipient private creatorFinderFeeRecipient1;
-    /// @dev Creator finder fee recipient 2
-    FeeRecipient private creatorFinderFeeRecipient2;
-
-    /// @dev Buyer finder fee, amount. Can be spread across several addresses (which are specified per tx)
-    uint256 private buyerFinderFee;
 
     /*///////////////////////////////////////////////////////////////
                     Constructor + initializer logic
@@ -125,21 +94,12 @@ contract OpenEditionCustomFee is
         _setupRole(_transferRole, _defaultAdmin);
         _setupRole(_transferRole, address(0));
 
-        initFees(_fees);
+        _setupRariFees(_fees);
         _setupDefaultRoyaltyInfo(_royaltyRecipient, _royaltyBps);
         _setupPrimarySaleRecipient(_saleRecipient);
 
         transferRole = _transferRole;
         minterRole = _minterRole;
-    }
-
-    function initFees(Fees memory _fees) internal {
-        protocolFee = _fees.protocolFee;
-        protocolFeeRecipient = _fees.protocolFeeRecipient;
-        creatorFinderFee = _fees.creatorFinderFee;
-        creatorFinderFeeRecipient1 = _fees.creatorFinderFeeRecipient1;
-        creatorFinderFeeRecipient2 = _fees.creatorFinderFeeRecipient2;
-        buyerFinderFee = _fees.buyerFinderFee;
     }
 
     /*///////////////////////////////////////////////////////////////
@@ -173,153 +133,9 @@ contract OpenEditionCustomFee is
         return _startTokenId();
     }
 
-    /// @dev Lets an account claim tokens.
-    function claim(
-        address _receiver,
-        uint256 _quantity,
-        address _currency,
-        uint256 _pricePerToken,
-        AllowlistProof calldata _allowlistProof,
-        bytes memory _data
-    ) public payable virtual override {
-        _beforeClaim(_receiver, _quantity, _currency, _pricePerToken, _allowlistProof, _data);
-
-        uint256 activeConditionId = getActiveClaimConditionId();
-
-        verifyClaim(activeConditionId, _dropMsgSender(), _quantity, _currency, _pricePerToken, _allowlistProof);
-
-        // Update contract state.
-        claimCondition.conditions[activeConditionId].supplyClaimed += _quantity;
-        claimCondition.supplyClaimedByWallet[activeConditionId][_dropMsgSender()] += _quantity;
-
-        // If there's a price, collect price.
-        _collectPriceOnClaim(address(0), _quantity, _currency, _pricePerToken, _data);
-
-        // Mint the relevant tokens to claimer.
-        uint256 startTokenId = _transferTokensOnClaim(_receiver, _quantity);
-
-        emit TokensClaimed(activeConditionId, _dropMsgSender(), _receiver, startTokenId, _quantity);
-
-        _afterClaim(_receiver, _quantity, _currency, _pricePerToken, _allowlistProof, _data);
-    }
-
     /*///////////////////////////////////////////////////////////////
                         Internal functions
     //////////////////////////////////////////////////////////////*/
-
-    /// @dev Collects and distributes the primary sale value of NFTs being claimed.
-    function _collectPriceOnClaim(
-        address _primarySaleRecipient,
-        uint256 _quantityToClaim,
-        address _currency,
-        uint256 _pricePerToken,
-        bytes memory _data
-    ) internal {
-        // _data format = <buyerFinder: address><buyerFinderFeeBps: uint96> X 2 (buyerFinderFeeBps - share, specified in bps)
-        // _data is optional. if present should be in the defined format. Can be 1 or 2 finder fees specified
-        // sum should be 10000 or tx is reverted otherwise
-
-        // price structure:
-        //   protocol fee, read from storage. on contract creation is read from static address
-        //   creator finder fee, read from storage, set on contract creation
-        //     2 creator finder fees are supported
-        //   buyer finder fee, amount is specified in storage (set on creation), shares are specified at the tx level
-        //     any number of finder fees are supported
-        //   if buyer finders are not specified, then buyer finder fee is paid to the protocol fee recipient
-        //   rest goes to creator (seller)
-        //
-        // constraint: price >= (protocol fee + sum(creator finder fees) + sum(buyer finder fees))
-
-        if (_pricePerToken == 0) {
-            require(msg.value == 0, "!Value");
-            return;
-        }
-
-        uint256 totalPrice = _quantityToClaim * _pricePerToken;
-
-        // Fees. fees - total number of fees already paid
-        uint256 fees = protocolFee * _quantityToClaim;
-
-        // Protocol fees
-        CurrencyTransferLib.transferCurrency(_currency, _msgSender(), protocolFeeRecipient, fees);
-
-        // Creator finder fees
-        uint _creatorFinderFee = creatorFinderFee;
-        if (_creatorFinderFee != 0) {
-            _transferCreatorFinderFee(_currency, _creatorFinderFee * _quantityToClaim);
-            fees += _creatorFinderFee * _quantityToClaim;
-        }
-
-        // Buyer finder fees
-        uint _buyerFinderFee = buyerFinderFee;
-        if (_buyerFinderFee != 0) {
-            _transferBuyerFinderFee(_currency, _buyerFinderFee * _quantityToClaim, _data);
-            fees += _buyerFinderFee * _quantityToClaim;
-        }
-
-        require(totalPrice >= fees, "price less than fees");
-
-        bool validMsgValue;
-        if (_currency == CurrencyTransferLib.NATIVE_TOKEN) {
-            validMsgValue = msg.value == totalPrice;
-        } else {
-            validMsgValue = msg.value == 0;
-        }
-        require(validMsgValue, "!V");
-
-        address saleRecipient = _primarySaleRecipient == address(0) ? primarySaleRecipient() : _primarySaleRecipient;
-
-        CurrencyTransferLib.transferCurrency(_currency, _msgSender(), saleRecipient, totalPrice - fees);
-    }
-
-    function _transferCreatorFinderFee(address _currency, uint256 _creatorFinderFee) internal {
-        FeeRecipient memory creatorFeeRecipient1 = creatorFinderFeeRecipient1;
-        uint creatorFeeRecipient1Amount = _creatorFinderFee * creatorFeeRecipient1.value / MAX_BPS;
-        CurrencyTransferLib.transferCurrency(_currency, _msgSender(), creatorFeeRecipient1.recipient, creatorFeeRecipient1Amount);
-
-        if (creatorFeeRecipient1.value < MAX_BPS) {
-            FeeRecipient memory creatorFeeRecipient2 = creatorFinderFeeRecipient2;
-            uint creatorFeeRecipient2Amount = _creatorFinderFee * creatorFeeRecipient2.value / MAX_BPS;
-            CurrencyTransferLib.transferCurrency(_currency, _msgSender(), creatorFeeRecipient2.recipient, creatorFeeRecipient2Amount);
-        }
-    }
-
-    function _transferBuyerFinderFee(address _currency, uint256 _buyerFinderFee, bytes memory _data) internal {
-        if (_data.length == 0) {
-            CurrencyTransferLib.transferCurrency(_currency, _msgSender(), protocolFeeRecipient, _buyerFinderFee);
-        } else {
-            if (_data.length == 32) {
-                address recipient;
-                uint bps;
-                assembly {
-                    recipient := shr(96, mload(add(_data, 32)))
-                    bps := shr(160, mload(add(_data, 52)))
-                }
-                require(bps == MAX_BPS, "!BuyerFeeBpsNot10000");
-                CurrencyTransferLib.transferCurrency(_currency, _msgSender(), recipient, _buyerFinderFee);
-            } else if (_data.length == 64) {
-                address recipient1;
-                uint bps1;
-                address recipient2;
-                uint bps2;
-                assembly {
-                    recipient1 := shr(96, mload(add(_data, 32)))
-                    bps1 := shr(160, mload(add(_data, 52)))
-                    recipient2 := shr(96, mload(add(_data, 64)))
-                    bps2 := shr(160, mload(add(_data, 84)))
-                }
-                require(bps1 + bps2 == MAX_BPS, "!BuyerFeeBpsNot10000");
-                CurrencyTransferLib.transferCurrency(_currency, _msgSender(), recipient1, (_buyerFinderFee) * bps1 / MAX_BPS);
-                CurrencyTransferLib.transferCurrency(_currency, _msgSender(), recipient2, (_buyerFinderFee) * bps2 / MAX_BPS);
-            } else {
-                revert("!DataLength");
-            }
-        }
-    }
-
-    function _collectPriceOnClaim(address, uint256, address, uint256) internal override {
-        revert();
-    }
 
     /// @dev Transfers the NFTs being claimed.
     function _transferTokensOnClaim(
--- projects/drop/test/open-edition.test.ts
+++ projects/drop/test/open-edition.test.ts
@@ -4,22 +4,23 @@ import {
 	EIP173Proxy,
 	EIP173Proxy__factory,
 	IDrop,
-	OpenEditionCustomFee,
-	OpenEditionCustomFee__factory
+	OpenEditionRariFees,
+	OpenEditionRariFees__factory
 } from "../typechain-types"
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers"
 import { expect } from "chai"
 import { randomAddress } from "hardhat/internal/hardhat-network/provider/utils/random"
+import { RariFeesDrop } from "../typechain-types/contracts/OpenEditionRariFees"
+import FeesStruct = RariFeesDrop.FeesStruct
 
-type FeesStruct = OpenEditionCustomFee.FeesStruct
 type AllowlistProofStruct = IDrop.AllowlistProofStruct
 
 describe("OpenEdition", () => {
-	let factory: OpenEditionCustomFee__factory
-	let impl: OpenEditionCustomFee
+	let factory: OpenEditionRariFees__factory
+	let impl: OpenEditionRariFees
 	let proxyFactory: EIP173Proxy__factory
 	let proxy: EIP173Proxy
-	let drop: OpenEditionCustomFee
+	let drop: OpenEditionRariFees
 	let first: SignerWithAddress
 	let signers: SignerWithAddress[]
 	let protocolRecipient: string
@@ -32,7 +33,7 @@ describe("OpenEdition", () => {
 		signers = await hre.ethers.getSigners()
 		first = signers[0]
 
-		factory = await hre.ethers.getContractFactory("OpenEditionCustomFee")
+		factory = await hre.ethers.getContractFactory("OpenEditionRariFees")
 		impl = await factory.deploy()
 		proxyFactory = await hre.ethers.getContractFactory("EIP173Proxy")
 	})
@@ -51,7 +52,7 @@ describe("OpenEdition", () => {
 	it("should send buyerFee to protocol if not specified", async () => {
 		await prepareDropContract()
 
-		await drop.claim(signers[5].address, 1, ETH_CURRENCY, 1000000000, proof, "0x", {
+		await drop.claim(signers[5].address, 1, ETH_CURRENCY, 1000000000, proof(), "0x", {
 			value: 1000000000
 		})
 
@@ -66,7 +67,7 @@ describe("OpenEdition", () => {
 		await prepareDropContract()
 
 		const data = `0x${buyerFinderFeeRecipient1.substring(2)}000000000000000000002710`
-		await drop.claim(signers[5].address, 1, ETH_CURRENCY, 1000000000, proof, data, {
+		await drop.claim(signers[5].address, 1, ETH_CURRENCY, 1000000000, proof(), data, {
 			value: 1000000000
 		})
 
@@ -81,7 +82,7 @@ describe("OpenEdition", () => {
 		await prepareDropContract()
 
 		const data = `0x${buyerFinderFeeRecipient1.substring(2)}000000000000000000001770${buyerFinderFeeRecipient2.substring(2)}000000000000000000000FA0`
-		await drop.claim(signers[5].address, 1, ETH_CURRENCY, 1000000000, proof, data, {
+		await drop.claim(signers[5].address, 1, ETH_CURRENCY, 1000000000, proof(), data, {
 			value: 1000000000
 		})
 
@@ -92,26 +93,72 @@ describe("OpenEdition", () => {
 		await expect(getBalance(creatorFinderFeeRecipient2)).eventually.to.equal(0)
 	})
 
-	async function prepareDropContract(secondCreatorFinderFeeBps: number = 0) {
-		await initialize(
+	it("should revert if price less than fees", async () => {
+		const price = 100
+		await prepareDropContract({ pricePerToken: price })
+		await expect(drop.claim(signers[5].address, 1, ETH_CURRENCY, price, proof(price), "0x", overrides(1000000)))
+		 	.to.eventually.be.rejectedWith("price less than fees")
+	})
+
+	it("should revert if total bps != 10000 for buyer finder fee", async () => {
+		const price = 1000000
+		await prepareDropContract({ pricePerToken: price })
+
+		const data = `0x${buyerFinderFeeRecipient1.substring(2)}000000000000000000002700`
+
+		await expect(drop.claim(signers[5].address, 1, ETH_CURRENCY, price, proof(price), data, overrides(1000000)))
+			.to.eventually.be.rejectedWith("!BuyerFeeBpsNot10000")
+	})
+
+	it("should revert if total bps != 10000 for creator finder fee", async () => {
+		const config = {}
+		const p = initialize(
 			{
 				protocolFee: 100,
 				protocolFeeRecipient: protocolRecipient,
 				creatorFinderFee: 150,
 				creatorFinderFeeRecipient1: {
 					recipient: creatorFinderFeeRecipient1,
-					value: 10000 - secondCreatorFinderFeeBps,
+					value: 5000,
 				},
 				creatorFinderFeeRecipient2: {
 					recipient: creatorFinderFeeRecipient2,
-					value: secondCreatorFinderFeeBps,
+					value: 10000,
 				},
 				buyerFinderFee: 200,
-			}
+			}, config
+		)
+
+		await expect(p).to.eventually.be.rejectedWith("!CreatorFeeBpsNot10000")
+	})
+
+	type DropContractsConfig = {
+		protocolFee?: number
+		secondCreatorFinderFeeBps?: number
+		pricePerToken?: number
+	}
+
+	async function prepareDropContract(config?: DropContractsConfig) {
+		config = config || {}
+		await initialize(
+			{
+				protocolFee: config.protocolFee || 100,
+				protocolFeeRecipient: protocolRecipient,
+				creatorFinderFee: 150,
+				creatorFinderFeeRecipient1: {
+					recipient: creatorFinderFeeRecipient1,
+					value: 10000 - (config.secondCreatorFinderFeeBps || 0),
+				},
+				creatorFinderFeeRecipient2: {
+					recipient: creatorFinderFeeRecipient2,
+					value: (config.secondCreatorFinderFeeBps || 0),
+				},
+				buyerFinderFee: 200,
+			}, config
 		)
 	}
 
-	async function initialize(fees: FeesStruct) {
+	async function initialize(fees: FeesStruct, config: DropContractsConfig) {
 		await drop.initialize(
 			first.address,
 			"TST",
@@ -138,7 +185,7 @@ describe("OpenEdition", () => {
 				supplyClaimed: 0,
 				quantityLimitPerWallet: 1000000,
 				merkleRoot: "0x0000000000000000000000000000000000000000000000000000000000000000",
-				pricePerToken: 1000000000,
+				pricePerToken: config.pricePerToken || 1000000000,
 				currency: ETH_CURRENCY,
 				metadata: "ipfs://QmVu98eczZRpSYcF3UKYRDkHsM2RMQR62KUYmk29UDbWTP/0"
 			}
@@ -152,9 +199,15 @@ describe("OpenEdition", () => {
 
 const ETH_CURRENCY = "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE"
 
-const proof: AllowlistProofStruct = {
-	proof: ["0x0000000000000000000000000000000000000000000000000000000000000000"],
-	currency: ETH_CURRENCY,
-	pricePerToken: 1000000000,
-	quantityLimitPerWallet: 1000000
+function proof(price: number = 1000000000): AllowlistProofStruct {
+	return {
+		proof: ["0x0000000000000000000000000000000000000000000000000000000000000000"],
+		currency: ETH_CURRENCY,
+		pricePerToken: price,
+		quantityLimitPerWallet: 1000000
+	}
+}
+
+function overrides(value: number) {
+	return { value }
 }
