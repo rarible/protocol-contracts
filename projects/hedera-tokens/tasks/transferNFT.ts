8ce0bea1bfb96a6d2fcc142904b071754579b6e0
ifelsedeveloper <xboxfadeev@gmail.com>
2025-03-14 14:14:45 +0000

    feat: fix transfer

--- projects/hedera-tokens/tasks/transferNFT.ts
+++ projects/hedera-tokens/tasks/transferNFT.ts
@@ -3,7 +3,6 @@ import { IERC721, IERC721__factory } from "../typechain-types";
 
 task("transferNFT", "Mint a non-fungible token with fix fee using the precompiled contract")
   .addParam("tokenAddress", "The address of the token to mint")
-  .addParam("tokenURI", "The URI of the token to mint")
   .addParam("to", "The address of the recipient")
   .addParam("tokenId", "The token id to transfer")
   .setAction(async (params, hre) => {
@@ -15,7 +14,7 @@ task("transferNFT", "Mint a non-fungible token with fix fee using the precompile
     const tokenAddress = params.tokenAddress;
     // https://console.filebase.com/object/rarible-drops/Eclipse/cat/metadata/1.json
     // QmYVnaTG4Fo5kPxkuZCCUp7B5XT5P88aARQe44WcxYs11r
-    const mintTx = await erc721.transferFrom(deployer.address, params.to, params.tokenId);
+    const mintTx = await erc721.transferFrom(deployer.address, params.to, params.tokenId, {gasLimit: 6_000_000});
     const txReceipt = await mintTx.wait();
     console.log("Mint tx hash", mintTx.hash);
     
