d7f6cc7e0281fe5be5a579d5713fd27042c57e10
ifelsedeveloper <xboxfadeev@gmail.com>
2025-04-11 11:07:32 +0100

    feat: add new drop721 deploy

--- projects/thirdweb-tokens/sh-examples/deploy-drop721.sh
+++ projects/thirdweb-tokens/sh-examples/deploy-drop721.sh
@@ -1,14 +1,14 @@
 npx hardhat deploy-drop721 --network polygon_mainnet \
-    --defaultAdmin 0xb3D6b3e65C39C5C3c7434a27542912C4600e9910 \
-    --name "MyDrop721Collection" \
-    --symbol "DRP" \
-    --contractUri "ipfs://QmSomeHash" \
-    --saleRecipient 0xfb571F9da71D1aC33E069571bf5c67faDCFf18e4 \
-    --royaltyRecipient 0xfb571F9da71D1aC33E069571bf5c67faDCFf18e4 \
-    --royaltyBps 500 \
-    --platformFeeBps 200 \
-    --platformFeeRecipient 0xfb571F9da71D1aC33E069571bf5c67faDCFf18e4 \
+    --default-admin 0xb3D6b3e65C39C5C3c7434a27542912C4600e9910 \
+    --name "DROP Test 721" \
+    --symbol "DRP721" \
+    --contract-uri "ipfs://QmdHaufjUDJgbZzZ4eFCjtJQyeQpuNwoEvqLm5rq159vC8/5" \
+    --sale-recipient 0xfb571F9da71D1aC33E069571bf5c67faDCFf18e4 \
+    --royalty-recipient 0xfb571F9da71D1aC33E069571bf5c67faDCFf18e4 \
+    --royalty-bps 500 \
+    --platform-fee-bps 200 \
+    --platform-fee-recipient 0xfb571F9da71D1aC33E069571bf5c67faDCFf18e4 \
     --salt 0x0000000000000000000000000000000000000000000000000000000000000001 \
-    --extraData 0x \
-    --cloneFactory 0x25548ba29a0071f30e4bdcd98ea72f79341b07a1 \
-    --implementation 0x123456789abcdef123456789abcdef123456789a
+    --extra-data 0x \
+    --clone-factory 0x25548ba29a0071f30e4bdcd98ea72f79341b07a1 \
+    --implementation 0xe6bb91e8facf3717d5f28093efc8e0fe00994cb1
