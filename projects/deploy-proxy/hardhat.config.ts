bef2cd9f6729b339a16d043b33beb94b592e509d
ifelsedeveloper <xboxfadeev@gmail.com>
2025-04-21 14:47:38 +0100

    feat: add deploy proxy and TW clone factory

---
+++ projects/deploy-proxy/hardhat.config.ts
@@ -0,0 +1,107 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-deploy-immutable-proxy";
+import "@openzeppelin/hardhat-upgrades";
+import "@nomiclabs/hardhat-truffle5";
+
+import * as dotenv from "dotenv";
+
+import "./tasks";
+
+dotenv.config();
+
+import {
+  loadApiKeys,
+  loadCustomNetworks,
+  loadNetworkConfigs,
+} from "@rarible/deploy-utils";
+
+const config: HardhatUserConfig = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.7.6",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+      {
+        version: "0.5.10",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 99999,
+          },
+          evmVersion: "petersburg",
+        },
+      },
+      {
+        version: "0.8.20",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+    overrides: {
+      "contracts/ImmutableCreate2Factory.sol": {
+        version: "0.5.10",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 99999,
+          }
+        },
+      },
+      "contracts/TWCloneFactory.sol": {
+        version: "0.8.20",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 99999,
+          }
+        },
+      },
+      "contracts/lib/soladity/src/utils/LibClone.sol": {
+        version: "0.8.20",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 99999,
+          }
+        },
+      }
+    },
+    settings: {
+      metadata: {
+        // Not including the metadata hash
+        // https://github.com/paulrberg/hardhat-template/issues/31
+        bytecodeHash: "none",
+      },
+      // Disable the optimizer when debugging
+      // https://hardhat.org/hardhat-network/#solidity-optimizer-support
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  namedAccounts: {
+    deployer: 0,
+  },
+  paths: {
+    sources: "contracts",
+  },
+  networks: loadNetworkConfigs(),
+  etherscan: {
+    apiKey: loadApiKeys(),
+    customChains: loadCustomNetworks(),
+  }
+};
+
+export default config;
---
+++ projects/deploy-proxy/sh-examples/deploy-drop721.sh
@@ -0,0 +1,33 @@
+npx hardhat deploy-drop721 --network polygon_mainnet \
+    --default-admin 0xb3D6b3e65C39C5C3c7434a27542912C4600e9910 \
+    --name "DROP Test 721" \
+    --symbol "DRP721" \
+    --contract-uri "ipfs://QmdHaufjUDJgbZzZ4eFCjtJQyeQpuNwoEvqLm5rq159vC8/5" \
+    --sale-recipient 0xfb571F9da71D1aC33E069571bf5c67faDCFf18e4 \
+    --royalty-recipient 0xfb571F9da71D1aC33E069571bf5c67faDCFf18e4 \
+    --royalty-bps 500 \
+    --platform-fee-bps 200 \
+    --platform-fee-recipient 0xfb571F9da71D1aC33E069571bf5c67faDCFf18e4 \
+    --salt 0x0000000000000000000000000000000000000000000000000000000000000001 \
+    --extra-data 0x \
+    --clone-factory 0x25548ba29a0071f30e4bdcd98ea72f79341b07a1 \
+    --implementation 0xe6bb91e8facf3717d5f28093efc8e0fe00994cb1
+
+
+    0x98C2d878064dCD20489214cf0866f972f91784D0
+
+    
+npx hardhat deploy-drop721 --network megaeth_testnet \
+    --default-admin 0x98556b192f8304001986e0bb94e61e51049a600c \
+    --name "DROP Test 721" \
+    --symbol "DRP721" \
+    --contract-uri "ipfs://QmdHaufjUDJgbZzZ4eFCjtJQyeQpuNwoEvqLm5rq159vC8/5" \
+    --sale-recipient 0x98556b192f8304001986e0bb94e61e51049a600c \
+    --royalty-recipient 0x98556b192f8304001986e0bb94e61e51049a600c \
+    --royalty-bps 500 \
+    --platform-fee-bps 200 \
+    --platform-fee-recipient 0x98556b192f8304001986e0bb94e61e51049a600c \
+    --salt 0x0000000000000000000000000000000000000000000000000000000000000001 \
+    --extra-data 0x \
+    --clone-factory 0x25548ba29a0071f30e4bdcd98ea72f79341b07a1 \
+    --implementation 0x98C2d878064dCD20489214cf0866f972f91784D0
