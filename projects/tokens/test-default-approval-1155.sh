97c892068aae4551a93bbdf94f51ca3de4ef630c
ifelsedeveloper <xboxfadeev@gmail.com>
2025-04-30 15:17:47 +0100

    feat: fix tests

--- projects/tokens/test-default-approval-1155.sh
+++ projects/tokens/test-default-approval-1155.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-truffle test ./test/erc-1155/DefaultApproval.test.js ./test/contracts/erc-1155/ERC1155DefaultApprovalTest.sol
+truffle test ./test/erc-1155/DefaultApproval.test.js ./test/contracts/erc-1155/ERC1155DefaultApprovalTest.sol ./test/contracts/TestERC1271.sol
--- projects/tokens/test-default-approval-721.sh
+++ projects/tokens/test-default-approval-721.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 truffle test ./test/erc-721-minimal/DefaultApproval.test.js \
-            ./test/contracts/erc-721/ERC721DefaultApprovalTest.sol 
+            ./test/contracts/erc-721/ERC721DefaultApprovalTest.sol \
+            ./test/contracts/TestERC1271.sol
