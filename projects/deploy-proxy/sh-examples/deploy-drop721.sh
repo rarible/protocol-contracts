49814d9b92948634d8760ac29aea54068257c2e3
ifelsedeveloper <xboxfadeev@gmail.com>
2025-05-05 13:47:02 +0100

    feat: add hoodi deploy

--- projects/deploy-proxy/sh-examples/deploy-drop721.sh
+++ projects/deploy-proxy/sh-examples/deploy-drop721.sh
@@ -29,3 +29,18 @@ npx hardhat deploy-drop721 --network megaeth_testnet \
     --extra-data 0x \
     --clone-factory 0x25548ba29a0071f30e4bdcd98ea72f79341b07a1 \
     --implementation 0x70682de037C28b46dB85408A5Cb9dF95e15a6301
+
+npx hardhat deploy-drop721 --network moonbeam \
+    --default-admin 0x5b04Bd4CF6ac8F976a1d578b7156206b781ca861 \
+    --name "DROP Test 721" \
+    --symbol "DRP721" \
+    --contract-uri "ipfs://QmdHaufjUDJgbZzZ4eFCjtJQyeQpuNwoEvqLm5rq159vC8/5" \
+    --sale-recipient 0x98556b192f8304001986e0bb94e61e51049a600c \
+    --royalty-recipient 0x98556b192f8304001986e0bb94e61e51049a600c \
+    --royalty-bps 500 \
+    --platform-fee-bps 200 \
+    --platform-fee-recipient 0x98556b192f8304001986e0bb94e61e51049a600c \
+    --salt 0x0000000000000000000000000000000000000000000000000000000000000021 \
+    --extra-data 0x \
+    --clone-factory 0x25548ba29a0071f30e4bdcd98ea72f79341b07a1 \
+    --implementation 0x70682de037C28b46dB85408A5Cb9dF95e15a6301
