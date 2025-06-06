2255cb10da0555915e2b9507f4c70490a4c4f4af
ifelsedeveloper <xboxfadeev@gmail.com>
2025-06-06 14:32:21 +0100

    feat: add nonce check

--- projects/deploy-proxy/deploy/02-deploy-rarible-create-x.ts
+++ projects/deploy-proxy/deploy/02-deploy-rarible-create-x.ts
@@ -1,10 +1,11 @@
+import { ethers } from "hardhat";
 import { DeployFunction, DeployResult } from "hardhat-deploy/dist/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 
 const deployLock: DeployFunction = async (
     hre: HardhatRuntimeEnvironment
 ) => {
-    const { deploy } = hre.deployments;
+    const { deploy, } = hre.deployments;
     const { getNamedAccounts } = hre;
     const {deployer,} = await getNamedAccounts();
     console.log('deployer', deployer)
@@ -15,6 +16,16 @@ const deployLock: DeployFunction = async (
         waitConfirmations: 1,
         nonce: 0, // nonce must be 0
     });
+    // Retrieve the full transaction to verify its nonce
+    const tx = await ethers.provider.getTransaction(
+        deployResult.transactionHash!
+    );
+
+    console.log("deploy tx nonce", tx.nonce); // should print 0
+
+    if (tx.nonce !== 0) {
+        throw new Error(`Expected nonce 0, got ${tx.nonce}`);
+    }
     console.log("transactionHash", deployResult.transactionHash)
 };
 
