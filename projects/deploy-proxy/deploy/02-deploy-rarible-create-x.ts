a636ee487bcb9e25a73a1ce2675a507ae5f651cd
GonzaloMBustos <54861063+GonzaloMBustos@users.noreply.github.com>
2025-06-06 10:39:32 -0300

    feat: CreateX Factory and tests (#349)
    * feat: CreateX Factory and tests
    
    * fix: contract name
    
    * feat: restore interface
    
    * feat: add deploy script
    
    * feat: fix deploy script
    
    * feat: add nonce check
    
    ---------
    
    Co-authored-by: Gonzalo Bustos <gbustos@spacedev.io>
    Co-authored-by: ifelsedeveloper <xboxfadeev@gmail.com>

---
+++ projects/deploy-proxy/deploy/02-deploy-rarible-create-x.ts
@@ -0,0 +1,33 @@
+import { ethers } from "hardhat";
+import { DeployFunction, DeployResult } from "hardhat-deploy/dist/types";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+
+const deployLock: DeployFunction = async (
+    hre: HardhatRuntimeEnvironment
+) => {
+    const { deploy, } = hre.deployments;
+    const { getNamedAccounts } = hre;
+    const {deployer,} = await getNamedAccounts();
+    console.log('deployer', deployer)
+    
+    const deployResult: DeployResult = await deploy("RaribleCreateX", {
+        from: deployer,
+        log: true,
+        waitConfirmations: 1,
+        nonce: 0, // nonce must be 0
+    });
+    // Retrieve the full transaction to verify its nonce
+    const tx = await ethers.provider.getTransaction(
+        deployResult.transactionHash!
+    );
+
+    console.log("deploy tx nonce", tx.nonce); // should print 0
+
+    if (tx.nonce !== 0) {
+        throw new Error(`Expected nonce 0, got ${tx.nonce}`);
+    }
+    console.log("transactionHash", deployResult.transactionHash)
+};
+
+export default deployLock;
+deployLock.tags = ["02", "RaribleCreateX"];
