2765a37e9a7764ef64d9e9117f13f7a046512b65
Gonzalo Bustos <gbustos@spacedev.io>
2025-06-12 15:06:43 -0300

    fix: missing import

--- projects/exchange-v2/contracts/test/ImportAll.sol
+++ projects/exchange-v2/contracts/test/ImportAll.sol
@@ -8,3 +8,5 @@ import "../../test/contracts/RaribleTransferManagerTest.sol";
 import "../../test/contracts/TestRoyaltiesRegistry.sol";
 import "../../test/contracts/TestERC721RoyaltiesV2.sol";
 import "../../test/contracts/ExchangeSimpleV2.sol";
+import "../../test/contracts/ERC721LazyMintTransferProxyTest.sol";
+import "../../test/contracts/ERC1155LazyMintTransferProxyTest.sol";
