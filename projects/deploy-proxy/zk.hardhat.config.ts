2b5c9c4cd57ebb17010588b779eb27dcf6f1ffe3
ifelsedeveloper <xboxfadeev@gmail.com>
2025-06-24 13:15:15 +0100

    feat: add zk support

--- projects/deploy-proxy/zk.hardhat.config.ts
+++ projects/deploy-proxy/zk.hardhat.config.ts
@@ -1,10 +1,12 @@
-import "@matterlabs/hardhat-zksync";
-import "@typechain/hardhat"; // Add TypeChain plugin
-
-// upgradable plugin
+import "@matterlabs/hardhat-zksync-solc";
+import "@matterlabs/hardhat-zksync-deploy";
+import "@matterlabs/hardhat-zksync-verify";
+import "@matterlabs/hardhat-zksync-ethers";
+import "zksync-ethers";
+import "@typechain/hardhat";
 
 import type {
-    HttpNetworkUserConfig, HardhatUserConfig
+  HttpNetworkUserConfig, HardhatUserConfig
 } from "hardhat/types";
 import * as dotenv from "dotenv";
 import * as os from "os";
@@ -22,17 +24,26 @@ function getConfigPath() {
 }
 
 function createNetwork(name: string): HttpNetworkUserConfig {
-    const configPath = path.join(getConfigPath(), name + ".json");
-    if (fs.existsSync(configPath)) {
-      var json = require(configPath);
-      if (json.verify && json.verify.apiUrl && json.verify.apiUrl.endsWith("/api")) {
-        json.verify.apiUrl = json.verify.apiUrl.slice(0, -4);
+  const configPath = path.join(getConfigPath(), name + ".json");
+  if (fs.existsSync(configPath)) {
+    var json = require(configPath);
+    if (json.verify && json.verify.apiUrl && json.verify.apiUrl.endsWith("/api")) {
+      json.verify.apiUrl = json.verify.apiUrl.slice(0, -4);
+    }
+    //if frame
+    if (!json.key) {
+      return {
+        url: json.url || "",
+        chainId: json.network_id,
+        timeout: 60000,
       }
+    } else {
+      // if not frame
       return {
         from: json.address,
         gasPrice: "auto",
         chainId: parseInt(json.network_id),
-        url: json.url,
+        url: json.url || "",
         accounts: [json.key],
         gas: "auto",
         saveDeployments: true,
@@ -46,18 +57,19 @@ function createNetwork(name: string): HttpNetworkUserConfig {
           : null,
         zksync: json.zksync === true,
       } as HttpNetworkUserConfig;
-    } else {
-      // File doesn't exist in path
-      return {
-        from: "0x0000000000000000000000000000000000000000",
-        gas: 0,
-        chainId: 0,
-        url: "",
-        accounts: [],
-        gasPrice: 0,
-      };
     }
+  } else {
+    // File doesn't exist in path
+    return {
+      from: "0x0000000000000000000000000000000000000000",
+      gas: 0,
+      chainId: 0,
+      url: "",
+      accounts: [],
+      gasPrice: 0,
+    };
   }
+}
 
 const config: HardhatUserConfig = {
   zksolc: {
@@ -74,15 +86,6 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: "0.7.6",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 200,
-          },
-        },
-      },
-      {
         version: "0.8.20",
         settings: {
           optimizer: {
@@ -93,27 +96,31 @@ const config: HardhatUserConfig = {
       },
     ],
     overrides: {
-      "contracts/TWCloneFactory.sol": {
-        version: "0.8.20",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 99999,
-          }
-        },
+    },
+    settings: {
+      metadata: {
+        // Not including the metadata hash
+        // https://github.com/paulrberg/hardhat-template/issues/31
+        bytecodeHash: "none",
       },
-      "contracts/lib/soladity/src/utils/LibClone.sol": {
-        version: "0.8.20",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 99999,
-          }
-        },
-      }
+      // Disable the optimizer when debugging
+      // https://hardhat.org/hardhat-network/#solidity-optimizer-support
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
     },
   },
-  defaultNetwork: "zksync_sepolia",
+  defaultNetwork: "zksync",
+  namedAccounts: {
+    deployer: 0,
+    // deployer: "ledger://0xe223825497c435BAeaf318F03d33Ec704954028A",
+    // zkcandy: "ledger://0xe223825497c435BAeaf318F03d33Ec704954028A",
+  },
+  paths: {
+    sources: "contracts",
+    artifacts: "artifacts-zk"
+  },
   networks: {
     sepolia: {
       zksync: false,
@@ -125,12 +132,46 @@ const config: HardhatUserConfig = {
       ...createNetwork("zksync_sepolia"),
       verifyURL: 'https://explorer.sepolia.era.zksync.dev/contract_verification'
     },
+    zksync: {
+      ...createNetwork("zksync"),
+      ethNetwork: "mainnet", // The Ethereum Web3 RPC URL, or the identifier of the network (e.g. `mainnet` or `sepolia`)
+      zksync: true
+    },
+    zkLinkGoerliTestnet: {
+      zksync: true,
+      ethNetwork: "goerli",
+      ...createNetwork("zkLinkGoerliTestnet"),
+      verifyURL: 'https://goerli.explorer.zklink.io/contracts/verify',
+      timeout: 120000
+    },
+    zkLink: {
+      zksync: true,
+      ethNetwork: "mainnet",
+      ...createNetwork("zkLink"),
+      verifyURL: 'https://explorer.zklink.io/contracts/verify'
+    },
+    zkcandy_sepolia: {
+      zksync: true,
+      ethNetwork: "sepolia",
+      ...createNetwork("zkcandy_sepolia"),
+      verifyURL: 'https://explorer.sepolia.era.zksync.dev/contract_verification'
+    },
+    zkcandy: {
+      ...createNetwork("zkcandy"),
+      zksync: true,
+      ethNetwork: "mainnet",
+    },
+    abstract_testnet: {
+      ...createNetwork("abstract_testnet"),
+    },
+    abstract: {
+      zksync: true,
+      ethNetwork: "mainnet",
+      ...createNetwork("abstract"),
+    }
   },
   etherscan: {
     apiKey: "P78HUI9K9SAM5QKD6ABU91G3CPDS98MZW2"
-  },
-  paths: {
-    
   },
   typechain: {
     outDir: "typechain-types-zk", // Output directory for generated typings
