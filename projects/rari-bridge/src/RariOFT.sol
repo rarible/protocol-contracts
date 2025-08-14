bb2afe4037de7303ce449cb542e9e9a5ead02de9
ifelsedeveloper <xboxfadeev@gmail.com>
2025-08-14 12:43:02 +0100

    feat: add prettier

--- projects/rari-bridge/src/RariOFT.sol
+++ projects/rari-bridge/src/RariOFT.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.22;
 
-import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
-import { OFT } from "@layerzerolabs/oft-evm/contracts/OFT.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {OFT} from "@layerzerolabs/oft-evm/contracts/OFT.sol";
 
 contract RariOFT is OFT {
     constructor(address _layerZeroEndpoint, address _delegate) OFT("Rarible Token", "RARI", _layerZeroEndpoint, _delegate) Ownable(_delegate) {}
