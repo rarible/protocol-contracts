95ef1b9ee6ffe043383d58a22b93df26edaec311
Vadim Fadeev <xboxfadeev@gmail.com>
2025-08-20 18:00:35 +0100

    Feature/pt 7307 somnia (#383)
    add somnia deploy + bridge

---
+++ projects/rari-bridge/tasks/wire.ts
@@ -0,0 +1,29 @@
+import { task } from "hardhat/config";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { getLedgerSigner } from "@rarible/deploy-utils";
+
+import { addressToBytes32 } from "../utils";
+import { EndpointId, EndpointVersion, getNetworkForChainId, networkToEndpointId } from "@layerzerolabs/lz-definitions";
+import { getEndpointV2IdByChainId } from "../utils";
+
+task("wire", "Set peer for RARI OFT bridge")
+  .addParam("contract", "Contract address (Adapter or OFT)")
+  .addParam("target", "Target contract address on remote chain")
+  .addParam("targetChainId", "Target endpoint ID")
+  .setAction(async (taskArgs, hre: HardhatRuntimeEnvironment) => {
+    const { OFTCore, OFTCore__factory } = await import("../typechain-types");
+    const { ethers } = hre;
+    const oft: OFTCore = OFTCore__factory.connect(taskArgs.contract, getLedgerSigner(ethers.provider, "m/44'/60'/0'/0/0")); // OFTCore ABI for setPeer
+
+    const targetBytes32 = addressToBytes32(taskArgs.target);
+
+    // // 1) Figure out the LayerZero "network" name from the EVM chainId
+    // //    e.g. 8453 -> { chainName: 'base', env: 'mainnet', ... } -> 'base-mainnet'
+    const chainIdNum = Number(taskArgs.targetChainId);
+    const targetEndpointId = getEndpointV2IdByChainId(chainIdNum);
+
+    const tx = await oft.setPeer(targetEndpointId, targetBytes32);
+    await tx.wait();
+
+    console.log(`Peer set: ${taskArgs.target} on eid ${targetEndpointId} for chainId ${chainIdNum}`);
+  });
