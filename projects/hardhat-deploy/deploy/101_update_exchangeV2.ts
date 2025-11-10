8b16079b81fa22d49372b32b4c43c644c91c6ebc
Vadim Fadeev <xboxfadeev@gmail.com>
2025-11-10 11:30:10 +0000

    feat: add AA support for orders (#399)
    * feat: add AA support for orders
    
    * feat: update exchange on base
    
    * feat: add new deploy

--- projects/hardhat-deploy/deploy/101_update_exchangeV2.ts
+++ projects/hardhat-deploy/deploy/101_update_exchangeV2.ts
@@ -32,4 +32,4 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 };
 
 export default func;
-func.tags = ['update-exchange'];
+func.tags = ['update-exchange', '101'];
