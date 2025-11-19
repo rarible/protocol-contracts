a302ae0b1cfa397d43be888c16cf679af65995f4
ifelsedeveloper <xboxfadeev@gmail.com>
2025-11-19 16:13:40 +0000

    feat: add rari and match

--- projects/hardhat-deploy/deploy/101_update_exchangeV2.ts
+++ projects/hardhat-deploy/deploy/101_update_exchangeV2.ts
@@ -4,7 +4,7 @@ import { DeployFunction } from 'hardhat-deploy/types';
 import { getConfig } from '../utils/utils'
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const { deploy } = hre.deployments;
+  const { deploy, execute } = hre.deployments;
   const { deployer } = await hre.getNamedAccounts();
   const { deploy_meta, deploy_non_meta } = getConfig(hre.network.name);
 
@@ -27,8 +27,29 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     },
     log: true,
     autoMine: true,
-    gasPrice: "100000000000", // 100 gwei
+    //gasPrice: "100000000000", // 100 gwei
+    gasLimit: 4000000,
   });
+
+  const proxyAdmin = await hre.deployments.get("DefaultProxyAdmin");
+  const proxyAdminContract = await hre.ethers.getContractAt("hardhat-deploy-immutable-proxy/solc_0.8/openzeppelin/proxy/transparent/ProxyAdmin.sol:ProxyAdmin", proxyAdmin.address);
+  
+  const exchangeV2ProxyAddress = (await hre.deployments.get(contractName + "_Proxy")).address;
+  const exchangeV2ImplementationAddress =  (await hre.deployments.get(contractName + "_Implementation")).address;
+  if( (await proxyAdminContract.getProxyImplementation(exchangeV2ProxyAddress)) !== exchangeV2ImplementationAddress) {
+    console.log("ExchangeV2 implementation is not the same as the implementation address, upgrading...");
+    await execute(
+      "DefaultProxyAdmin",
+      { from: deployer, log: true },
+      "upgrade",
+      exchangeV2ProxyAddress, 
+      exchangeV2ImplementationAddress
+    );
+    await proxyAdminContract.upgrade(exchangeV2ProxyAddress, exchangeV2ImplementationAddress);
+    console.log("Upgraded ExchangeV2 implementation to: ", exchangeV2ImplementationAddress);
+  } else {
+    console.log("ExchangeV2 implementation is the same as the implementation address, no upgrade needed");
+  }
 
 };
 
