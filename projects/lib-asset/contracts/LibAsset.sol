77a0a9ff5ab2f4dd819facfa9308a153806f0cea
ifelsedeveloper <xboxfadeev@gmail.com>
2025-11-22 20:34:03 +0000

    feat: add lib-asset

--- projects/lib-asset/contracts/LibAsset.sol
+++ projects/lib-asset/contracts/LibAsset.sol
@@ -1,22 +1,19 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.7.6;
+pragma solidity 0.8.30;
 
 library LibAsset {
-    bytes4 constant public ETH_ASSET_CLASS = bytes4(keccak256("ETH"));
-    bytes4 constant public ERC20_ASSET_CLASS = bytes4(keccak256("ERC20"));
-    bytes4 constant public ERC721_ASSET_CLASS = bytes4(keccak256("ERC721"));
-    bytes4 constant public ERC1155_ASSET_CLASS = bytes4(keccak256("ERC1155"));
-    bytes4 constant public COLLECTION = bytes4(keccak256("COLLECTION"));
-    bytes4 constant public CRYPTO_PUNKS = bytes4(keccak256("CRYPTO_PUNKS"));
+    bytes4 public constant ETH_ASSET_CLASS = bytes4(keccak256("ETH"));
+    bytes4 public constant ERC20_ASSET_CLASS = bytes4(keccak256("ERC20"));
+    bytes4 public constant ERC721_ASSET_CLASS = bytes4(keccak256("ERC721"));
+    bytes4 public constant ERC1155_ASSET_CLASS = bytes4(keccak256("ERC1155"));
+    bytes4 public constant COLLECTION = bytes4(keccak256("COLLECTION"));
+    bytes4 public constant CRYPTO_PUNKS = bytes4(keccak256("CRYPTO_PUNKS"));
 
-    bytes32 constant ASSET_TYPE_TYPEHASH = keccak256(
-        "AssetType(bytes4 assetClass,bytes data)"
-    );
+    bytes32 constant ASSET_TYPE_TYPEHASH = keccak256("AssetType(bytes4 assetClass,bytes data)");
 
-    bytes32 constant ASSET_TYPEHASH = keccak256(
-        "Asset(AssetType assetType,uint256 value)AssetType(bytes4 assetClass,bytes data)"
-    );
+    bytes32 constant ASSET_TYPEHASH =
+        keccak256("Asset(AssetType assetType,uint256 value)AssetType(bytes4 assetClass,bytes data)");
 
     struct AssetType {
         bytes4 assetClass;
@@ -29,19 +26,20 @@ library LibAsset {
     }
 
     function hash(AssetType memory assetType) internal pure returns (bytes32) {
+        // prettier-ignore
         return keccak256(abi.encode(
-                ASSET_TYPE_TYPEHASH,
-                assetType.assetClass,
-                keccak256(assetType.data)
-            ));
+            ASSET_TYPE_TYPEHASH, 
+            assetType.assetClass, 
+            keccak256(assetType.data)
+        ));
     }
 
     function hash(Asset memory asset) internal pure returns (bytes32) {
+        // prettier-ignore
         return keccak256(abi.encode(
-                ASSET_TYPEHASH,
-                hash(asset.assetType),
-                asset.value
-            ));
+            ASSET_TYPEHASH, 
+            hash(asset.assetType), 
+            asset.value
+        ));
     }
-
 }
