66054f1175b8a6829bffcfc84d2f4abc8d82ae28
ifelsedeveloper <xboxfadeev@gmail.com>
2025-11-22 20:13:39 +0000

    feat: add lint in build

--- projects/lib-bp/contracts/BpLibrary.sol
+++ projects/lib-bp/contracts/BpLibrary.sol
@@ -3,7 +3,7 @@
 pragma solidity 0.8.30;
 
 library BpLibrary {
-  function bp(uint value, uint bpValue) internal pure returns (uint) {
-    return (value * bpValue) / 10000;
-  }
+    function bp(uint value, uint bpValue) internal pure returns (uint) {
+        return (value * bpValue) / 10000;
+    }
 }
