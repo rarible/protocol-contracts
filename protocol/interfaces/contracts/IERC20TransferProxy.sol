9e3d380885906669e47ffedc48fb19a676298277
ifelsedeveloper <xboxfadeev@gmail.com>
2025-11-23 15:33:51 +0000

    feat: fix compile issues

--- protocol/interfaces/contracts/IERC20TransferProxy.sol
+++ protocol/interfaces/contracts/IERC20TransferProxy.sol
@@ -2,8 +2,8 @@
 
 pragma solidity ^0.8.30;
 
-import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 interface IERC20TransferProxy {
-    function erc20safeTransferFrom(IERC20Upgradeable token, address from, address to, uint256 value) external;
+    function erc20safeTransferFrom(IERC20 token, address from, address to, uint256 value) external;
 }
--- protocol/interfaces/contracts/INftTransferProxy.sol
+++ protocol/interfaces/contracts/INftTransferProxy.sol
@@ -2,11 +2,11 @@
 
 pragma solidity ^0.8.30;
 
-import "@openzeppelin/contracts-upgradeable/token/ERC721/IERC721Upgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC1155/IERC1155Upgradeable.sol";
+import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import {IERC1155} from "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 
 interface INftTransferProxy {
-    function erc721safeTransferFrom(IERC721Upgradeable token, address from, address to, uint256 tokenId) external;
+    function erc721safeTransferFrom(IERC721 token, address from, address to, uint256 tokenId) external;
 
-    function erc1155safeTransferFrom(IERC1155Upgradeable token, address from, address to, uint256 id, uint256 value, bytes calldata data) external;
+    function erc1155safeTransferFrom(IERC1155 token, address from, address to, uint256 id, uint256 value, bytes calldata data) external;
 }
