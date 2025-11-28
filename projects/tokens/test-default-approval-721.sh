d953de239211091ce64021371cbea88e1872f688
ifelsedeveloper <xboxfadeev@gmail.com>
2025-11-28 12:55:37 +0000

    feat: add tests

--- projects/tokens/test-default-approval-721.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-truffle test ./test/erc-721-minimal/DefaultApproval.test.js \
-            ./test/contracts/erc-721/ERC721DefaultApprovalTest.sol 
--- projects/tokens/test-mint-721.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-truffle test ./test/erc-721-minimal/Mint721Validator.test.js \
-            ./test/contracts/erc-721/Mint721ValidatorTest.sol \
-            ./test/contracts/TestERC1271.sol 
