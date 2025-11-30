11d1652d2d155284bd0d9d76253083933843d8e8
ifelsedeveloper <xboxfadeev@gmail.com>
2025-11-30 22:42:50 +0000

    feat: add mint

---
+++ protocol/common-sdk/src/mint1155.ts
@@ -0,0 +1,44 @@
+// <ai_context> Helper function for signing ERC1155 lazy mint data. Matches logic from old Truffle scripts/mint1155.js and LibERC1155LazyMint's EIP712 type ordering (Mint1155(uint256 tokenId,uint256 supply,string tokenURI,Part[] creators,Part[] royalties)). </ai_context>
+
+import type * as ethersTypes from "ethers";
+
+export async function sign(
+  signer: ethersTypes.Signer,
+  tokenId: bigint,
+  tokenURI: string,
+  supply: bigint,
+  creators: Array<{ account: string; value: bigint }>,
+  royalties: Array<{ account: string; value: bigint }>,
+  verifyingContract: string,
+) {
+  const domain = {
+    name: "Mint1155",
+    version: "1",
+    chainId: (await signer.provider?.getNetwork())?.chainId,
+    verifyingContract,
+  };
+
+  const types = {
+    Part: [
+      { name: "account", type: "address" },
+      { name: "value", type: "uint96" },
+    ],
+    Mint1155: [
+      { name: "tokenId", type: "uint256" },
+      { name: "supply", type: "uint256" },
+      { name: "tokenURI", type: "string" },
+      { name: "creators", type: "Part[]" },
+      { name: "royalties", type: "Part[]" },
+    ],
+  };
+
+  const value = {
+    tokenId,
+    supply,
+    tokenURI,
+    creators,
+    royalties,
+  };
+
+  return signer.signTypedData(domain, types, value);
+}
