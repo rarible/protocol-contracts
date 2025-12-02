9346da85d1230c3c67ca22106a98b4d85c9aaf44
ifelsedeveloper <xboxfadeev@gmail.com>
2025-12-02 01:14:10 +0000

    feat: update sol

--- protocol/exchange-v2/test/contracts/TestRoyaltiesRegistry.sol
+++ protocol/exchange-v2/test/contracts/TestRoyaltiesRegistry.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.7.6;
-pragma abicoder v2;
+pragma solidity ^0.8.30;
 
 import "@rarible/exchange-interfaces/contracts/IRoyaltiesProvider.sol";
 import "@rarible/royalties/contracts/LibRoyaltiesV2.sol";
