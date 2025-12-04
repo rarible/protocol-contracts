706293e789b2e779630fb23f8d58a02969e4ac70
ifelsedeveloper <xboxfadeev@gmail.com>
2025-12-04 10:46:56 +0000

    feat: fix deploy

--- protocol/hardhat-v3-deploy/deploy/003_deploy_ExchangeV2_CollectionMatcher.ts
+++ protocol/hardhat-v3-deploy/deploy/003_deploy_ExchangeV2_CollectionMatcher.ts
@@ -4,6 +4,14 @@ import { ERC721_LAZY, ERC1155_LAZY, COLLECTION, getConfig, ROYALTIES_REGISTRY_TY
 
 const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000';
 
+// Helper to create a deployment object with address and abi
+function createDeployment(address: string, artifact: any) {
+	return {
+		address,
+		abi: artifact.abi,
+	};
+}
+
 export default deployScript(
 	async ({deployViaProxy, deploy, execute, get, namedAccounts, network}) => {
 		const {deployer} = namedAccounts;
@@ -99,7 +107,7 @@ async function deployAndSetupExchange({
 					0,
 					deployer,
 					royaltiesRegistryAddress,
-          deployer,
+					deployer,
 				],
 			},
 			proxyContract: 'SharedAdminOptimizedTransparentProxy',
@@ -107,47 +115,49 @@ async function deployAndSetupExchange({
 		}
 	);
 
+	// Create deployment objects with address and abi for execute calls
+	const transferProxyWithAbi = createDeployment(transferProxyAddress, artifacts.TransferProxy);
+	const erc20TransferProxyWithAbi = createDeployment(erc20TransferProxyAddress, artifacts.ERC20TransferProxy);
+	const erc721LazyMintTransferProxyWithAbi = createDeployment(erc721LazyMintTransferProxyAddress, artifacts.ERC721LazyMintTransferProxy);
+	const erc1155LazyMintTransferProxyWithAbi = createDeployment(erc1155LazyMintTransferProxyAddress, artifacts.ERC1155LazyMintTransferProxy);
+	const exchangeV2WithAbi = createDeployment(exchangeV2Deployment.address, artifacts.ExchangeV2);
+
 	// Add exchangeV2 as operator to all 4 transfer proxies
+	// Using execute with deployment object (not string name) to include abi
 	console.log('Adding exchangeV2 as operator to all 4 transfer proxies');
-	await execute(
-		'TransferProxy',
-		{account: deployer},
-		'addOperator',
-		[exchangeV2Deployment.address]
-	);
-	await execute(
-		'ERC20TransferProxy',
-		{account: deployer},
-		'addOperator',
-		[exchangeV2Deployment.address]
-	);
-	await execute(
-		'ERC721LazyMintTransferProxy',
-		{account: deployer},
-		'addOperator',
-		[exchangeV2Deployment.address]
-	);
-	await execute(
-		'ERC1155LazyMintTransferProxy',
-		{account: deployer},
-		'addOperator',
-		[exchangeV2Deployment.address]
-	);
+	await execute(transferProxyWithAbi, {
+		account: deployer,
+		functionName: 'addOperator',
+		args: [exchangeV2Deployment.address],
+	});
+	await execute(erc20TransferProxyWithAbi, {
+		account: deployer,
+		functionName: 'addOperator',
+		args: [exchangeV2Deployment.address],
+	});
+	await execute(erc721LazyMintTransferProxyWithAbi, {
+		account: deployer,
+		functionName: 'addOperator',
+		args: [exchangeV2Deployment.address],
+	});
+	await execute(erc1155LazyMintTransferProxyWithAbi, {
+		account: deployer,
+		functionName: 'addOperator',
+		args: [exchangeV2Deployment.address],
+	});
 
 	// Set 2 lazy transfer proxies in exchangeV2 contract (other 2 are set in initialiser)
 	console.log('Setting 2 lazy transfer proxies in exchangeV2 contract');
-	await execute(
-		contractName,
-		{account: deployer},
-		'setTransferProxy',
-		[ERC721_LAZY, erc721LazyMintTransferProxyAddress]
-	);
-	await execute(
-		contractName,
-		{account: deployer},
-		'setTransferProxy',
-		[ERC1155_LAZY, erc1155LazyMintTransferProxyAddress]
-	);
+	await execute(exchangeV2WithAbi, {
+		account: deployer,
+		functionName: 'setTransferProxy',
+		args: [ERC721_LAZY, erc721LazyMintTransferProxyAddress],
+	});
+	await execute(exchangeV2WithAbi, {
+		account: deployer,
+		functionName: 'setTransferProxy',
+		args: [ERC1155_LAZY, erc1155LazyMintTransferProxyAddress],
+	});
 
 	// Deploy and setup collection matcher (no proxy needed)
 	console.log('Deploying and setting collection matcher');
@@ -160,10 +170,9 @@ async function deployAndSetupExchange({
 		},
 	});
 
-	await execute(
-		contractName,
-		{account: deployer},
-		'setAssetMatcher',
-		[COLLECTION, assetMatcherCollectionDeployment.address]
-	);
+	await execute(exchangeV2WithAbi, {
+		account: deployer,
+		functionName: 'setAssetMatcher',
+		args: [COLLECTION, assetMatcherCollectionDeployment.address],
+	});
 }
