814848488551be88e727327d5290c7abc3a4f1b6
contractor <contractor@rarible.com>
2026-02-08 12:29:03 +0000

    feat: add deploy-airdrop functionality with associated scripts and contract

---
+++ projects/drops/sh-examples/deploy-airdrop.sh
@@ -0,0 +1,18 @@
+npx hardhat deploy-airdrop --network polygon_mainnet \
+    --default-admin 0xb3D6b3e65C39C5C3c7434a27542912C4600e9910 \
+    --contract-uri "ipfs://QmdHaufjUDJgbZzZ4eFCjtJQyeQpuNwoEvqLm5rq159vC8/5" \
+    --salt 0x0000000000000000000000000000000000000000000000000000000000000001 \
+    --extra-data 0x \
+    --clone-factory 0x9A611f52a1b8007B1A20935ba619352C694fAE8F \
+    --implementation 0x0000000000000000000000000000000000000000
+
+
+# megaeth
+
+npx hardhat deploy-airdrop --network megaeth_testnet \
+    --default-admin 0x98556b192f8304001986e0bb94e61e51049a600c \
+    --contract-uri "ipfs://QmdHaufjUDJgbZzZ4eFCjtJQyeQpuNwoEvqLm5rq159vC8/5" \
+    --salt 0x0000000000000000000000000000000000000000000000000000000000000001 \
+    --extra-data 0x \
+    --clone-factory 0x9A611f52a1b8007B1A20935ba619352C694fAE8F \
+    --implementation 0x0000000000000000000000000000000000000000
